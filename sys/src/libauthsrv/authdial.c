a703dfcbe4e8caa44e188c5d10d842c1ce09b4a2
pad <pad@fb.com>
2014-01-16 21:09:08 -0800

    * sys/src/libauthsrv/authdial.c: comment code so can compile

--- sys/src/libauthsrv/authdial.c
+++ sys/src/libauthsrv/authdial.c
@@ -2,35 +2,35 @@
 #include <libc.h>
 #include <authsrv.h>
 #include <bio.h>
-#include <ndb.h>
+//PAD #include <ndb.h>
 
 int
 authdial(char *netroot, char *dom)
 {
-	char *p;
-	int rv;
-
-	if(dom == nil)
-		/* look for one relative to my machine */
-		return dial(netmkaddr("$auth", netroot, "ticket"), 0, 0, 0);
-
-	/* look up an auth server in an authentication domain */
-	p = csgetvalue(netroot, "authdom", dom, "auth", nil);
-
-	/* if that didn't work, just try the IP domain */
-	if(p == nil)
-		p = csgetvalue(netroot, "dom", dom, "auth", nil);
-	/*
-	 * if that didn't work, try p9auth.$dom.  this is very helpful if
-	 * you can't edit /lib/ndb.
-	 */
-	if(p == nil)
-		p = smprint("p9auth.%s", dom);
-	if(p == nil){			/* should no longer ever happen */
-		werrstr("no auth server found for %s", dom);
-		return -1;
-	}
-	rv = dial(netmkaddr(p, netroot, "ticket"), 0, 0, 0);
-	free(p);
-	return rv;
+//PAD: 	char *p;
+//PAD: 	int rv;
+//PAD: 
+//PAD: 	if(dom == nil)
+//PAD: 		/* look for one relative to my machine */
+//PAD: 		return dial(netmkaddr("$auth", netroot, "ticket"), 0, 0, 0);
+//PAD: 
+//PAD: 	/* look up an auth server in an authentication domain */
+//PAD: 	p = csgetvalue(netroot, "authdom", dom, "auth", nil);
+//PAD: 
+//PAD: 	/* if that didn't work, just try the IP domain */
+//PAD: 	if(p == nil)
+//PAD: 		p = csgetvalue(netroot, "dom", dom, "auth", nil);
+//PAD: 	/*
+//PAD: 	 * if that didn't work, try p9auth.$dom.  this is very helpful if
+//PAD: 	 * you can't edit /lib/ndb.
+//PAD: 	 */
+//PAD: 	if(p == nil)
+//PAD: 		p = smprint("p9auth.%s", dom);
+//PAD: 	if(p == nil){			/* should no longer ever happen */
+//PAD: 		werrstr("no auth server found for %s", dom);
+//PAD: 		return -1;
+//PAD: 	}
+//PAD: 	rv = dial(netmkaddr(p, netroot, "ticket"), 0, 0, 0);
+//PAD: 	free(p);
+//PAD: 	return rv;
 }
