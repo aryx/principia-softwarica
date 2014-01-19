2099b3546f60f51484dc51c2fb6e61de3f8bf209
pad <pad@fb.com>
2014-01-19 11:54:47 -0800

    * env.sh: need add . in path, for 8._cp to work

--- env.sh
+++ env.sh
@@ -1,7 +1,7 @@
 export KENCC=/home/pad/kencc
 export PLAN9=/usr/local/plan9
 
-export PATH=$PLAN9/bin:$KENCC/bin:$PATH
+export PATH=$PLAN9/bin:$KENCC/bin:$PATH:.
 export MKSHELL=$PLAN9/bin/rc
 
 export objtype=386
