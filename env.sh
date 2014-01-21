7108434a40d91b65710350ecdcc6da10d2db69f1
pad <pad@fb.com>
2014-01-21 12:19:52 -0800

    * env.sh: fix

--- env.sh
+++ env.sh
@@ -1,8 +1,12 @@
 export KENCC=/home/pad/kencc
 export PLAN9=/usr/local/plan9
 
-export PATH=$PLAN9/bin:$KENCC/bin:$PATH:.
+export PATH=$PLAN9/bin:$KENCC/bin:$PATH
+#todo: does not work :( have to put this line in all mkfile
 export MKSHELL=$PLAN9/bin/rc
+
+#for 8._cp to be found and called
+PATH=$PATH:.
 
 export objtype=386
 export cputype=386
