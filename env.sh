d3b266294338ba722ba07f6149cd00e04b9a5ae6
pad <pad@fb.com>
2014-01-28 19:39:27 -0800

    get rid of MKSHELL

--- env.sh
+++ env.sh
@@ -1,9 +1,8 @@
 export KENCC=/home/pad/kencc
+# need to modify plan9/src/cmd/mk/shell.c and put rcshell as default shell
 export PLAN9=/usr/local/plan9
 
 export PATH=$PLAN9/bin:$KENCC/bin:$PATH
-#todo: does not work :( have to put this line in all mkfile
-export MKSHELL=$PLAN9/bin/rc
 
 #for 8._cp to be found and called
 PATH=$PATH:.
