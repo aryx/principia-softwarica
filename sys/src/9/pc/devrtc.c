ec25e6e07d5b1b11cc09c152b8f7a16f8ad552d2
pad <pad@fb.com>
2014-03-13 16:52:40 -0700

    * sys/src/9/pc/: adjust symlinks (use xmv)

--- sys/src/9/pc/devrtc.c
+++ sys/src/9/pc/devrtc.c
@@ -1,1 +1,1 @@
-/Users/yoann.padioleau/github/xix/xix-plan9/kernel/processes/devrtc.c
+/Users/yoann.padioleau/github/xix/xix-plan9/kernel/devices/devrtc.c
--- sys/src/9/port/devproc.c
+++ sys/src/9/port/devproc.c
@@ -1,1 +1,1 @@
-/Users/yoann.padioleau/github/xix/xix-plan9/kernel/processes/devproc.c
+/Users/yoann.padioleau/github/xix/xix-plan9/kernel/devices/devproc.c
