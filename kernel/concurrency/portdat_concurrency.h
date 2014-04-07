28593501faabdf37c836b7854a5f36a55c863184
pad <pad@fb.com>
2014-04-07 09:10:42 -0700

    * kernel/concurrency/portdat_concurrency.h: extraize

--- kernel/concurrency/portdat_concurrency.h
+++ kernel/concurrency/portdat_concurrency.h
@@ -4,18 +4,23 @@
 struct KQLock
 {
 	Lock	use;		/* to access Qlock structure */
+
   // see also Proc.qnext for getting the full chain from head to tail.
 	Proc	*head;		/* next process waiting for object */
 	Proc	*tail;		/* last process waiting for object */
-	int	locked;		/* flag */
+
+	bool	locked;		/* flag */
 	uintptr	qpc;		/* pc of the holder */
 };
 
 struct RWlock
 {
 	Lock	use;
+
+  // see also Proc.qnext for getting the full chain from head to tail.
 	Proc	*head;		/* list of waiting processes */
 	Proc	*tail;
+
 	ulong	wpc;		/* pc of writer */
 	Proc	*wproc;		/* writing proc */
 	int	readers;	/* number of readers */
@@ -27,7 +32,6 @@ struct Ref
 	Lock;
 	long	ref;
 };
-
 
 
 // defined in this directory but no functions are operating on in this dir
@@ -42,10 +46,10 @@ struct Sema
 	Rendez;
 	long	*addr;
 	int	waiting;
+
 	Sema	*next;
 	Sema	*prev;
 };
-
 
 // see also Waitq in portdat_processes.h?
 // see also Rgrp, Fgrp, Pgrp in portdat_processes.h?  and Egrp in files.h?
