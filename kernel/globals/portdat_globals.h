73e9e2509ddc7e7c08acaa047b5974b5af6c4731
pad <pad@fb.com>
2014-04-08 12:59:12 -0700

    * kernel/globals/portdat_globals.h: typeify

--- kernel/globals/portdat_globals.h
+++ kernel/globals/portdat_globals.h
@@ -1,25 +1,29 @@
 
-extern Mach *m;
 extern Conf conf;
-extern Dev** devtab/*[]*/;
-extern char* eve;
-int iseve(void);
 
-
+// ref<Mach>, the actual Mach is where??
+extern Mach *m;
 /*
  * Each processor sees its own Mach structure at address MACHADDR.
  * However, the Mach structures must also be available via the per-processor
  * MMU information array machp, mainly for disambiguation and access to
  * the clock which is only maintained by the bootstrap processor (0).
  */
-// MAXMACH is defined in 386/mem.h
+// array<ref<Mach>>, MAXMACH is defined in 386/mem.h
 extern Mach* machp[MAXMACH];
 
-#define MACHP(n)  (machp[n])
+// array<Dev>, it looks like an allocated array<ref<dev>> but
+// it is really a static array put here to avoid backward deps on conf_devtab,
+// and it is not really a <ref<dev>> because it's pointer to static
+// structures (e.g. mousedevtab, vgadevtab, etc).
+extern Dev** devtab;
 
+extern char* eve;
+int iseve(void);
+
+#define MACHP(n)  (machp[n])
 // MACHADDR is defined in 386/mem.h
 // TODO: why not m->externup? m is not valid?
 #define up  (((Mach*)MACHADDR)->externup)
-
 #define poperror()    up->nerrlab--
 #define waserror()  (up->nerrlab++, setlabel(&up->errlab[up->nerrlab-1]))
