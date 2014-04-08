936ad54226bc48b6bfaf31466fe9131646661b36
pad <pad@fb.com>
2014-04-08 11:42:38 -0700

    * kernel/core/386/dat_core.h: untabity

--- kernel/core/386/dat_core.h
+++ kernel/core/386/dat_core.h
@@ -3,89 +3,89 @@
 
 struct Mach
 {
-	int	machno;			/* physical id of processor (KNOWN TO ASSEMBLY) */
+  int machno;     /* physical id of processor (KNOWN TO ASSEMBLY) */
   // must be second field at 0x04, used by splhi()
-	ulong	splpc;			/* pc of last caller to splhi */
+  ulong splpc;      /* pc of last caller to splhi */
 
-	ulong*	pdb;			/* page directory base for this processor (va) */
-	Tss*	tss;			/* tss for this processor */
-	Segdesc	*gdt;			/* gdt for this processor */
+  ulong*  pdb;      /* page directory base for this processor (va) */
+  Tss*  tss;      /* tss for this processor */
+  Segdesc *gdt;     /* gdt for this processor */
 
-	Proc*	proc;			/* current process on this processor */
-	Proc*	externup;		/* extern register Proc *up */
+  Proc* proc;     /* current process on this processor */
+  Proc* externup;   /* extern register Proc *up */
 
-	Page*	pdbpool;
-	int	pdbcnt;
+  Page* pdbpool;
+  int pdbcnt;
 
-	ulong	ticks;			/* of the clock since boot time */
-	Label	sched;			/* scheduler wakeup */
-	Lock	alarmlock;		/* access to alarm list */
-	void*	alarm;			/* alarms bound to this clock */
-	int	inclockintr;
+  ulong ticks;      /* of the clock since boot time */
+  Label sched;      /* scheduler wakeup */
+  Lock  alarmlock;    /* access to alarm list */
+  void* alarm;      /* alarms bound to this clock */
+  int inclockintr;
 
-	Proc*	readied;		/* for runproc */
-	ulong	schedticks;		/* next forced context switch */
+  Proc* readied;    /* for runproc */
+  ulong schedticks;   /* next forced context switch */
 
-	int	tlbfault;
-	int	tlbpurge;
-	int	pfault;
-	int	cs;
-	int	syscall;
-	int	load;
-	int	intr;
-	int	flushmmu;		/* make current proc flush it's mmu state */
-	int	ilockdepth;
-	Perf	perf;			/* performance counters */
+  int tlbfault;
+  int tlbpurge;
+  int pfault;
+  int cs;
+  int syscall;
+  int load;
+  int intr;
+  int flushmmu;   /* make current proc flush it's mmu state */
+  int ilockdepth;
+  Perf  perf;     /* performance counters */
 
-	ulong	spuriousintr;
-	int	lastintr;
+  ulong spuriousintr;
+  int lastintr;
 
-	int	loopconst;
+  int loopconst;
 
-	Lock	apictimerlock;
-	int	cpumhz;
-	uvlong	cyclefreq;		/* Frequency of user readable cycle counter */
-	uvlong	cpuhz;
-	int	cpuidax;
-	int	cpuiddx;
-	char	cpuidid[16];
-	char*	cpuidtype;
-	int	havetsc;
-	int	havepge;
-	uvlong	tscticks;
-	int	pdballoc;
-	int	pdbfree;
-	FPsave	*fpsavalign;
+  Lock  apictimerlock;
+  int cpumhz;
+  uvlong  cyclefreq;    /* Frequency of user readable cycle counter */
+  uvlong  cpuhz;
+  int cpuidax;
+  int cpuiddx;
+  char  cpuidid[16];
+  char* cpuidtype;
+  int havetsc;
+  int havepge;
+  uvlong  tscticks;
+  int pdballoc;
+  int pdbfree;
+  FPsave  *fpsavalign;
 
-	vlong	mtrrcap;
-	vlong	mtrrdef;
-	vlong	mtrrfix[11];
-	vlong	mtrrvar[32];		/* 256 max. */
+  vlong mtrrcap;
+  vlong mtrrdef;
+  vlong mtrrfix[11];
+  vlong mtrrvar[32];    /* 256 max. */
 
-	int	stack[1];
+  int stack[1];
 };
 
 
 struct Conf
 {
-	ulong	nmach;		/* processors */
-	ulong	nproc;		/* processes */
-	Confmem	mem[4];		/* physical memory */
+  ulong nmach;    /* processors */
+  ulong nproc;    /* processes */
+  Confmem mem[4];   /* physical memory */
 
-	ulong	monitor;	/* has monitor? */
+  ulong monitor;  /* has monitor? */
 
-	ulong	npage;		/* total physical pages of memory */
-	ulong	upages;		/* user page pool */
-	ulong	nimage;		/* number of page cache image headers */
-	ulong	nswap;		/* number of swap pages */
+  ulong npage;    /* total physical pages of memory */
+  ulong upages;   /* user page pool */
+  ulong nimage;   /* number of page cache image headers */
+  ulong nswap;    /* number of swap pages */
 
-	int	nswppo;		/* max # of pageouts per segment pass */
+  int nswppo;   /* max # of pageouts per segment pass */
 
-	ulong	base0;		/* base of bank 0 */
-	ulong	base1;		/* base of bank 1 */
+  ulong base0;    /* base of bank 0 */
+  ulong base1;    /* base of bank 1 */
 
-	ulong	copymode;	/* 0 is copy on write, 1 is copy on reference */
-	ulong	ialloc;		/* max interrupt time allocation in bytes */
-	ulong	pipeqsize;	/* size in bytes of pipe queues */
-	int	nuart;		/* number of uart devices */
+  ulong copymode; /* 0 is copy on write, 1 is copy on reference */
+  ulong ialloc;   /* max interrupt time allocation in bytes */
+  ulong pipeqsize;  /* size in bytes of pipe queues */
+  int nuart;    /* number of uart devices */
 };
