7214e89715da8be8a8cd050db14c9cda049dd407
pad <pad@fb.com>
2014-04-08 11:30:52 -0700

    * kernel/concurrency/386/dat_concurrency.h: untabity, typeify

--- kernel/concurrency/386/dat_concurrency.h
+++ kernel/concurrency/386/dat_concurrency.h
@@ -1,14 +1,19 @@
 
 struct Lock
 {
-	ulong	key; // 0 when unset, 0xDEADDEAD when acquired
-	ulong	sr; // saved priority level when using ilock() to restore in iunlock()
-	ulong	pc; // for debugging
-	Proc	*p; // the process locking should be the same unlocking
-	Mach	*m; // not that used, only in iprintcanlock apparently
-	ushort	isilock; // 0 when from lock(), 1 when from ilock()
+  ulong key; // 0 when unset, 0xDEADDEAD when acquired, could be a bool
+  bool_ushort isilock; // false when from lock(), true when from ilock()
 
-#ifdef LOCKCYCLES
-	long	lockcycles;
-#endif
+  ulong sr; // saved priority level when using ilock() to restore in iunlock()
+  ulong pc; // for debugging
+
+  // option<ref<Proc>>, None when key = 0
+  Proc  *p; // the process locking should be the same unlocking
+  // option<ref<Mach>>
+  Mach  *m; // not that used, only in iprintcanlock apparently
+
+
+//#ifdef LOCKCYCLES
+  long  lockcycles;
+//#endif
 };
