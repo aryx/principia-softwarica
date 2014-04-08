952c9a3edfe3f40c41d00eb1e9344086f8eca910
pad <pad@fb.com>
2014-04-08 11:40:21 -0700

    * kernel/processes/386/dat_processes.h: untabity

--- kernel/processes/386/dat_processes.h
+++ kernel/processes/386/dat_processes.h
@@ -2,8 +2,8 @@
 // things used in Mach (and Proc), =~ a jumpbuf in C, for coroutines
 struct Label
 {
-	ulong	sp; // virt_addr?
-	ulong	pc; // virt_addr?
+  ulong sp; // virt_addr?
+  ulong pc; // virt_addr?
 };
 
 /*
@@ -12,13 +12,13 @@ struct Label
 //used to be in portdat.h, but needed by Mach
 struct Perf
 {
-	ulong	intrts;		/* time of last interrupt */
-	ulong	inintr;		/* time since last clock tick in interrupt handlers */
-	ulong	avg_inintr;	/* avg time per clock tick in interrupt handlers */
-	ulong	inidle;		/* time since last clock tick in idle loop */
-	ulong	avg_inidle;	/* avg time per clock tick in idle loop */
-	ulong	last;		/* value of perfticks() at last clock tick */
-	ulong	period;		/* perfticks() per clock tick */
+  ulong intrts;   /* time of last interrupt */
+  ulong inintr;   /* time since last clock tick in interrupt handlers */
+  ulong avg_inintr; /* avg time per clock tick in interrupt handlers */
+  ulong inidle;   /* time since last clock tick in idle loop */
+  ulong avg_inidle; /* avg time per clock tick in idle loop */
+  ulong last;   /* value of perfticks() at last clock tick */
+  ulong period;   /* perfticks() per clock tick */
 };
 
 
@@ -28,57 +28,57 @@ struct Perf
  */
 enum
 {
-	/* this is a state */
-	FPinit=		0,
-	FPactive=	1,
-	FPinactive=	2,
+  /* this is a state */
+  FPinit=   0,
+  FPactive= 1,
+  FPinactive= 2,
 
-	/* the following is a bit that can be or'd into the state */
-	FPillegal=	0x100,
+  /* the following is a bit that can be or'd into the state */
+  FPillegal=  0x100,
 };
 
 //@Scheck: unnamed substructure
-struct	FPstate			/* x87 fpu state */
+struct  FPstate     /* x87 fpu state */
 {
-	ushort	control;
-	ushort	r1;
-	ushort	status;
-	ushort	r2;
-	ushort	tag;
-	ushort	r3;
-	ulong	pc;
-	ushort	selector;
-	ushort	r4;
-	ulong	operand;
-	ushort	oselector;
-	ushort	r5;
-	uchar	regs[80];	/* floating point registers */
+  ushort  control;
+  ushort  r1;
+  ushort  status;
+  ushort  r2;
+  ushort  tag;
+  ushort  r3;
+  ulong pc;
+  ushort  selector;
+  ushort  r4;
+  ulong operand;
+  ushort  oselector;
+  ushort  r5;
+  uchar regs[80]; /* floating point registers */
 };
 
 //@Scheck: unnamed substructure
-struct	FPssestate		/* SSE fp state */
+struct  FPssestate    /* SSE fp state */
 {
-	ushort	fcw;		/* control */
-	ushort	fsw;		/* status */
-	ushort	ftw;		/* tag */
-	ushort	fop;		/* opcode */
-	ulong	fpuip;		/* pc */
-	ushort	cs;		/* pc segment */
-	ushort	r1;		/* reserved */
-	ulong	fpudp;		/* data pointer */
-	ushort	ds;		/* data pointer segment */
-	ushort	r2;
-	ulong	mxcsr;		/* MXCSR register state */
-	ulong	mxcsr_mask;	/* MXCSR mask register */
-	uchar	xregs[480];	/* extended registers */
+  ushort  fcw;    /* control */
+  ushort  fsw;    /* status */
+  ushort  ftw;    /* tag */
+  ushort  fop;    /* opcode */
+  ulong fpuip;    /* pc */
+  ushort  cs;   /* pc segment */
+  ushort  r1;   /* reserved */
+  ulong fpudp;    /* data pointer */
+  ushort  ds;   /* data pointer segment */
+  ushort  r2;
+  ulong mxcsr;    /* MXCSR register state */
+  ulong mxcsr_mask; /* MXCSR mask register */
+  uchar xregs[480]; /* extended registers */
 };
 
 //@Scheck: unnamed substructure
-struct	SFPssestate		/* SSE fp state with alignment slop */
+struct  SFPssestate   /* SSE fp state with alignment slop */
 {
-	FPssestate;
-	uchar	alignpad[FPalign]; /* slop to allow copying to aligned addr */
-	ulong	magic;		/* debugging: check for overrun */
+  FPssestate;
+  uchar alignpad[FPalign]; /* slop to allow copying to aligned addr */
+  ulong magic;    /* debugging: check for overrun */
 };
 
 /*
@@ -86,8 +86,8 @@ struct	SFPssestate		/* SSE fp state with alignment slop */
  * port code assumes this.
  */
 union FPsave {
-	FPstate;
-	SFPssestate;
+  FPstate;
+  SFPssestate;
 };
 
 
@@ -99,21 +99,21 @@ union FPsave {
 //@Scheck: unnamed substructure
 struct Notsave
 {
-	ulong	svflags;
-	ulong	svcs;
-	ulong	svss;
+  ulong svflags;
+  ulong svcs;
+  ulong svss;
 };
 
 
 
 struct Active
 {
-	Lock;
-	int	machs;			/* bitmap of active CPUs */
-	int	exiting;		/* shutdown */
-	int	ispanic;		/* shutdown in response to a panic */
-	int	thunderbirdsarego;	/* lets the added processors continue to schedinit */
-	int	rebooting;		/* just idle cpus > 0 */
+  Lock;
+  int machs;      /* bitmap of active CPUs */
+  int exiting;    /* shutdown */
+  int ispanic;    /* shutdown in response to a panic */
+  int thunderbirdsarego;  /* lets the added processors continue to schedinit */
+  int rebooting;    /* just idle cpus > 0 */
 };
 
 
@@ -124,25 +124,25 @@ struct Active
 // this is actually only used in 386/ code.
 struct PCArch
 {
-	char*	id;
-	int	(*ident)(void);		/* this should be in the model */
-	void	(*reset)(void);		/* this should be in the model */
-	int	(*serialpower)(int);	/* 1 == on, 0 == off */
-	int	(*modempower)(int);	/* 1 == on, 0 == off */
+  char* id;
+  int (*ident)(void);   /* this should be in the model */
+  void  (*reset)(void);   /* this should be in the model */
+  int (*serialpower)(int);  /* 1 == on, 0 == off */
+  int (*modempower)(int); /* 1 == on, 0 == off */
 
-	void	(*intrinit)(void);
-	int	(*intrenable)(Vctl*);
-	int	(*intrvecno)(int);
-	int	(*intrdisable)(int);
-	void	(*introff)(void);
-	void	(*intron)(void);
+  void  (*intrinit)(void);
+  int (*intrenable)(Vctl*);
+  int (*intrvecno)(int);
+  int (*intrdisable)(int);
+  void  (*introff)(void);
+  void  (*intron)(void);
 
-	void	(*clockenable)(void);
-	uvlong	(*fastclock)(uvlong*);
-	void	(*timerset)(uvlong);
+  void  (*clockenable)(void);
+  uvlong  (*fastclock)(uvlong*);
+  void  (*timerset)(uvlong);
 
-	void	(*resetothers)(void);	/* put other cpus into reset */
+  void  (*resetothers)(void); /* put other cpus into reset */
 };
 
-extern PCArch	*arch;			/* PC architecture */
+extern PCArch *arch;      /* PC architecture */
 
