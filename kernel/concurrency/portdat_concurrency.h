b0a7c7786f60e49b709be14c4d9f26cfe2f245b6
pad <pad@fb.com>
2014-04-08 11:48:17 -0700

    * kernel/concurrency/portdat_concurrency.h: untabity

--- kernel/concurrency/portdat_concurrency.h
+++ kernel/concurrency/portdat_concurrency.h
@@ -6,42 +6,42 @@
 // Kernel basic lock with Queue (renamed to avoid ambiguity with libc.h Qlock)
 struct KQLock
 {
-	bool	locked;		/* flag */
+  bool  locked;   /* flag */
 
   // list<ref<Proc>> (next = Proc.qnext)
-	Proc	*head;		/* next process waiting for object */
+  Proc  *head;    /* next process waiting for object */
   // list<ref<Proc>> (next = Proc.??)
-	Proc	*tail;		/* last process waiting for object */
+  Proc  *tail;    /* last process waiting for object */
 
-	uintptr	qpc;		/* pc of the holder */ // for debugging?
+  uintptr qpc;    /* pc of the holder */ // for debugging?
 
-	Lock	use;		/* to access Qlock structure */
+  Lock  use;    /* to access Qlock structure */
 };
 
 // Read/Write lock
 struct RWlock
 {
-	int	readers;	/* number of readers */
-	int	writer;		/* number of writers */
+  int readers;  /* number of readers */
+  int writer;   /* number of writers */
 
   // list<ref<Proc>> (next = Proc.qnext)
-	Proc	*head;		/* list of waiting processes */
+  Proc  *head;    /* list of waiting processes */
   // list<ref<Proc>> (next = Proc.qnext)??
-	Proc	*tail;
+  Proc  *tail;
   // option<ref<Proc>>
-	Proc	*wproc;		/* writing proc */
+  Proc  *wproc;   /* writing proc */
 
-	ulong	wpc;		/* pc of writer */
+  ulong wpc;    /* pc of writer */
 
-	Lock	use;
+  Lock  use;
 };
 
 // For reference counting shared things (e.g. a Page)
 struct Ref
 {
-	long	ref;
+  long  ref;
 
-	Lock;
+  Lock;
 };
 
 
@@ -49,20 +49,20 @@ struct Ref
 struct Rendez
 {
   // ??
-	Proc	*p;
+  Proc  *p;
 
-	Lock;
+  Lock;
 };
 
 struct Sema
 {
-	long	*addr;
-	int	waiting;
+  long  *addr;
+  int waiting;
 
   //list<Sema> of ??
-	Sema	*next;
-	Sema	*prev;
-	Rendez;
+  Sema  *next;
+  Sema  *prev;
+  Rendez;
 };
 
 // see also Waitq in portdat_processes.h?
