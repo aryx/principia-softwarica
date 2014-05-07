e0ed807a75bfee5665348a6b0c75e7e649f3fec8
pad <pad@fb.com>
2014-05-07 14:18:46 -0700

    * kernel/files/portdat_files.h: reoder fields of Dev, cleaner

--- kernel/files/portdat_files.h
+++ kernel/files/portdat_files.h
@@ -289,25 +289,31 @@ struct Walkqid
 /*s: struct Dev */
 struct Dev
 {
-    int dc; // dev character code, e.g. #/ (devroot), #e (devenv), ...
+    int dc; // dev character code, e.g. '/' (devroot), 'e' (devenv), ...
     char* name;
     
     void  (*reset)(void);
     void  (*init)(void);
     void  (*shutdown)(void);
     Chan* (*attach)(char*);
+
     Walkqid*(*walk)(Chan*, Chan*, char**, int);
-    int (*stat)(Chan*, uchar*, int);
-    Chan* (*open)(Chan*, int);
+
     void  (*create)(Chan*, char*, int, ulong);
+    void  (*remove)(Chan*);
+
+    Chan* (*open)(Chan*, int);
     void  (*close)(Chan*);
     long  (*read)(Chan*, void*, long, vlong);
-    Block*  (*bread)(Chan*, long, ulong);
     long  (*write)(Chan*, void*, long, vlong);
+
+    Block* (*bread)(Chan*, long, ulong);
     long  (*bwrite)(Chan*, Block*, ulong);
-    void  (*remove)(Chan*);
+
+    int (*stat)(Chan*, uchar*, int);
     int (*wstat)(Chan*, uchar*, int);
-    void  (*power)(int);  /* power mgt: power(1) => on, power (0) => off */
+
+    void  (*power)(bool);  /* power mgt: power(1) => on, power (0) => off */
     int (*config)(int, char*, DevConf*);  /* returns nil on error */
 
     /*s: Dev other fields */
