b15dbd3f4b5ab9bd1abf73a56ab296aff4facbdc
pad <pad@fb.com>
2014-05-07 11:33:47 -0700

    * kernel/core/portdat.c: LPize globals

--- kernel/core/portdat.c
+++ kernel/core/portdat.c
@@ -9,15 +9,19 @@
 /*e: kernel basic includes */
 
 // was in init/main.c
+/*s: global conf */
 Conf conf;
+/*e: global conf */
 // bool cpuserver; // defined in $CONF.c
 
+/*s: globals confname and confvar */
 // conf (boot) parameters *e.g. { "*kernelpercent*" => "60" }
 // hash<string, string>
 char *confname[MAXCONF];
 char *confval[MAXCONF];
 // Hashtbl.length(confname)
 int nconf;
+/*e: globals confname and confvar */
 
 char* getconf(char *name)
 {
@@ -29,9 +33,20 @@ char* getconf(char *name)
 }
 
 // was in init/main.c
+/*s: global m */
 Mach *m;
+/*e: global m */
 // was in dat.h
+/*s: global machp */
+/*
+ * Each processor sees its own Mach structure at address MACHADDR.
+ * However, the Mach structures must also be available via the per-processor
+ * MMU information array machp, mainly for disambiguation and access to
+ * the clock which is only maintained by the bootstrap processor (0).
+ */
+// array<ref<Mach>>, MAXMACH is defined in 386/mem.h
 Mach* machp[MAXMACH];
+/*e: global machp */
 
 // was in security/auth.c
 char    *eve;
