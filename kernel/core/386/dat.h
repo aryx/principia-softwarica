69d02b2be430815fceea5b1b867db9c1d12ad764
pad <pad@fb.com>
2014-05-08 15:02:34 -0700

    * kernel/core/portdat.h: as well as portfns.h

--- kernel/core/386/dat.h
+++ kernel/core/386/dat.h
@@ -40,9 +40,8 @@ enum miscsize_dat {
 #include "dat_buses.h"
 #include "../port/portdat_buses.h"
 
+// defines consdevtab 
 #include "../port/portdat_console.h"
-
-#include "../port/portdat.h"
 
 /*s: constant AOUT_MAGIC */
 /*
@@ -69,4 +68,33 @@ enum misc_dat {
   Sse2  = 1<<26,  /* thus mfence & lfence instr.s */
 };
 /*e: enum misc_dat */
+
+// could be put in lib.h
+/*s: portdat.h macros */
+#define MIN(a, b) ((a) < (b)? (a): (b))
+#define HOWMANY(x, y) (((x)+((y)-1))/(y))
+#define ROUNDUP(x, y) (HOWMANY((x), (y))*(y)) /* ceiling */
+#define ROUND(s, sz)  (((s)+(sz-1))&~(sz-1))
+// BY2PG is defined in mem.h, which should always be included before "dat.h"!
+#define PGROUND(s)  ROUNDUP(s, BY2PG)
+/*e: portdat.h macros */
+
+// convenient constants
+/*s: enum miscsize_portdat */
+enum miscsize_portdat
+{
+  PRINTSIZE = 256,
+  NUMSIZE = 12,   /* size of formatted number */
+  MB =    (1024*1024),
+  /* READSTR was 1000, which is way too small for usb's ctl file */
+  READSTR = 4000,   /* temporary buffer size for device reads */
+};
+/*e: enum miscsize_portdat */
+
+/*s: portdat.h pragmas */
+#pragma varargck  type  "I" uchar*
+#pragma varargck  type  "V" uchar*
+#pragma varargck  type  "E" uchar*
+#pragma varargck  type  "M" uchar*
+/*e: portdat.h pragmas */
 /*e: dat.h */
--- kernel/core/386/fns.h
+++ kernel/core/386/fns.h
@@ -1,5 +1,16 @@
 /*s: fns.h */
-#include "../port/portfns.h"
+#include "../port/portfns_core.h"
+#include "../port/portfns_concurrency.h"
+#include "../port/portfns_memory.h"
+#include "../port/portfns_files.h"
+#include "../port/portfns_processes.h"
+#include "../port/portfns_misc.h"
+#include "../port/portfns_console.h"
+#include "../port/portfns_buses.h"
+#include "../port/portfns_devices.h"
+#include "../port/portfns_security.h"
+#include "../port/portfns_network.h"
+#include "../port/portfns_init.h"
 
 /*s: fns.h declarations */
 
