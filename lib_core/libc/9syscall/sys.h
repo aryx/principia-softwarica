639991d08d84049cdaa917452263e989c7e93570
pad <pad@fb.com>
2014-05-08 13:14:58 -0700

    * lib_core/libc/9syscall/sys.h: reordered!

--- lib_core/libc/9syscall/sys.h
+++ lib_core/libc/9syscall/sys.h
@@ -1,41 +1,53 @@
 #define	NOP		0
-#define	BIND		2
-#define	CHDIR		3
-#define	CLOSE		4
-#define	DUP		5
-#define	ALARM		6
-#define	EXEC		7
-#define	EXITS		8
-#define	FAUTH		10
-#define	SEGBRK		12
-#define	OPEN		14
-#define	OSEEK		16
-#define	SLEEP		17
-#define	RFORK		19
+
+#define	RFORK		1
+#define	EXEC		2
+#define	EXITS		3
+#define	SLEEP		4
+#define	ALARM		5
+
+#define	CREATE		6
+#define	REMOVE		7
+#define	OPEN		8
+#define	CLOSE		9
+#define	PREAD		10
+#define	PWRITE		11
+#define	SEEK		12
+#define	OSEEK		13
+#define	STAT		14
+#define	FSTAT		15
+#define	WSTAT		16
+#define	FWSTAT		17
+
+#define	CHDIR		18
+#define	FD2PATH		19
+#define	DUP		    20
 #define	PIPE		21
-#define	CREATE		22
-#define	FD2PATH		23
-#define	BRK_		24
-#define	REMOVE		25
-#define	NOTIFY		28
-#define	NOTED		29
-#define	SEGATTACH	30
-#define	SEGDETACH	31
-#define	SEGFREE		32
-#define	SEGFLUSH	33
-#define	RENDEZVOUS	34
-#define	UNMOUNT		35
-#define	SEMACQUIRE	37
-#define	SEMRELEASE	38
-#define	SEEK		39
-#define	FVERSION	40
-#define	ERRSTR		41
-#define	STAT		42
-#define	FSTAT		43
-#define	WSTAT		44
-#define	FWSTAT		45
-#define	MOUNT		46
-#define	AWAIT		47
-#define	PREAD		50
-#define	PWRITE		51
-#define	TSEMACQUIRE	52
+
+#define	BIND		22
+#define	MOUNT		23
+#define	UNMOUNT		24
+
+#define	AWAIT		25
+#define	RENDEZVOUS	26
+
+#define	SEMACQUIRE	27
+#define	SEMRELEASE	28
+#define	TSEMACQUIRE	29
+
+#define	NOTIFY		30
+#define	NOTED		31
+
+#define	BRK_		32
+
+#define	SEGATTACH	33
+#define	SEGDETACH	34
+#define	SEGFREE		35
+#define	SEGFLUSH	36
+#define	SEGBRK		37
+
+#define	FVERSION	38
+#define	FAUTH		39
+
+#define	ERRSTR		40
+
