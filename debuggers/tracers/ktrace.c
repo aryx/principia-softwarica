e0080f15fe68efcb4c326b4b6df179e5c02543c7
pad <pad@fb.com>
2014-05-27 18:11:56 -0700

    * debuggers/tracers/ktrace.c: remove code for other platforms

--- debuggers/tracers/ktrace.c
+++ debuggers/tracers/ktrace.c
@@ -1,20 +1,22 @@
 #include <u.h>
 #include <libc.h>
 #include <bio.h>
-#include <mach.h>
 #include <ctype.h>
 
+#include <mach.h>
+
+// ktrace - interpret kernel stack dumps
+
 static	int	rtrace(uvlong, uvlong, uvlong);
-static	int	ctrace(uvlong, uvlong, uvlong);
 static	int	i386trace(uvlong, uvlong, uvlong);
-static	int	amd64trace(uvlong, uvlong, uvlong);
+
 static	uvlong	getval(uvlong);
 static	void	inithdr(int);
 static	void	fatal(char*, ...);
 static	void	readstack(void);
 
 static	Fhdr	fhdr;
-static	int	interactive;
+static	int	interactive = 0;
 
 #define	FRAMENAME	".frame"
 
@@ -66,14 +68,14 @@ main(int argc, char *argv[])
 
 	ARGBEGIN{
 	case 'i':
-		interactive++;
+		interactive = 1;
 		break;
 	default:
 		usage();
 	}ARGEND
 
 	link = 0;
-	t = ctrace;
+	t = rtrace;
 	switch(argc){
 	case 4:
 		t = rtrace;
@@ -97,25 +99,11 @@ main(int argc, char *argv[])
 	case I_MAGIC:	/* intel 386 */
 		t = i386trace;
 		break;
-	case S_MAGIC:	/* amd64 */
-		t = amd64trace;
-		break;
-	case A_MAGIC:	/* 68020 */
-	case J_MAGIC:	/* intel 960 */
-		t = ctrace;
-		break;
-	case K_MAGIC:	/* sparc */
-	case D_MAGIC:	/* amd 29000 */
 	case V_MAGIC:	/* mips 3000 */
 	case M_MAGIC:	/* mips 4000 */
 	case E_MAGIC:	/* arm 7-something */
-	case Q_MAGIC:	/* powerpc */
 	case N_MAGIC:	/* mips 4000 LE */
-	case L_MAGIC:	/* dec alpha */
 		t = rtrace;
-		break;
-	case X_MAGIC:	/* att dsp 3210 */
-		sysfatal("can't ktrace %s", argv[0]);
 		break;
 	default:
 		fprint(2, "%s: warning: can't tell what type of stack %s uses; assuming it's %s\n",
@@ -137,6 +125,7 @@ inithdr(int fd)
 		fatal("%r\n");
 }
 
+// for MIPS and ARM
 static int
 rtrace(uvlong pc, uvlong sp, uvlong link)
 {
@@ -178,44 +167,6 @@ rtrace(uvlong pc, uvlong sp, uvlong link)
 }
 
 static int
-ctrace(uvlong pc, uvlong sp, uvlong link)
-{
-	Symbol s;
-	char buf[128];
-	int found;
-	uvlong opc, moved;
-	long j;
-
-	USED(link);
-	j = 0;
-	opc = 0;
-	while(pc && opc != pc) {
-		moved = pc2sp(pc);
-		if (moved == ~0){
-			print("pc2sp(%#.8llux) = -1 %r\n", pc);
-			break;
-		}
-		found = findsym(pc, CTEXT, &s);
-		if (!found){
-			print("findsym fails\n");
-			break;
-		}
-		symoff(buf, sizeof buf, pc, CANY);
-		fmt(buf, pc);
-
-		sp += moved;
-		opc = pc;
-		pc = getval(sp);
-		if(pc == 0)
-			break;
-		sp += mach->szaddr;	/*assumes address size = stack width*/
-		if(++j > 40)
-			break;
-	}
-	return j;
-}
-
-static int
 i386trace(uvlong pc, uvlong sp, uvlong link)
 {
 	int i;
@@ -231,20 +182,16 @@ i386trace(uvlong pc, uvlong sp, uvlong link)
 		symoff(buf, sizeof buf, pc, CANY);
 		fmt(buf, pc);
 
-//XXX		s.value &= ~(uintptr)0;
 		if(pc != s.value) {	/* not at first instruction */
 			if(findlocal(&s, FRAMENAME, &f) == 0)
 				break;
 			sp += f.value-mach->szaddr;
 		}else if(strcmp(s.name, "forkret") == 0){
-//XXX
 			print("//passing interrupt frame; last pc found at sp=%#llux\n", osp);
-
 			sp +=  15 * mach->szaddr;		/* pop interrupt frame */
 		}
 
 		pc = getval(sp);
-//XXX
 		if(pc == 0 && strcmp(s.name, "forkret") == 0){
 			sp += 3 * mach->szaddr;			/* pop iret eip, cs, eflags */
 			print("//guessing call through invalid pointer, try again at sp=%#llux\n", sp);
@@ -258,71 +205,8 @@ i386trace(uvlong pc, uvlong sp, uvlong link)
 		osp = sp;
 
 		sp += mach->szaddr;
-//XXX
 		if(strcmp(s.name, "forkret") == 0)
 			sp += 2 * mach->szaddr;			/* pop iret cs, eflags */
-
-		if(++i > 40)
-			break;
-	}
-	return i;
-}
-
-static int
-amd64trace(uvlong pc, uvlong sp, uvlong link)
-{
-	int i, isintrr;
-	uvlong osp;
-	Symbol s, f;
-	char buf[128];
-
-	USED(link);
-	i = 0;
-	osp = 0;
-	while(findsym(pc, CTEXT, &s)) {
-
-		symoff(buf, sizeof buf, pc, CANY);
-		fmt(buf, pc);
-
-		if(strcmp(s.name, "_intrr") == 0)
-			isintrr = 1;
-		else
-			isintrr = 0;
-		if(pc != s.value) {	/* not at first instruction */
-			if(findlocal(&s, FRAMENAME, &f) == 0)
-				break;
-			sp += f.value-mach->szaddr;
-		}
-		else if(isintrr){
-			print("//passing interrupt frame; last pc found at sp=%#llux\n", osp);
-			/*
-			 * Pop interrupt frame (ureg.h) up to the IP value.
-			 */
-			sp += 19 * mach->szaddr;
-		}
-
-		pc = getval(sp);
-		if(pc == 0 && isintrr){
-			/*
-			 * Pop IP, CS and FLAGS to get to the SP.
-			 * The AMD64 aligns the interrupt stack on
-			 * a 16-byte boundary so have the get the
-			 * SP from the saved frame.
-			 */
-			sp += 3 * mach->szaddr;
-			print("//guessing call through invalid pointer; try again at sp=%#llux\n", sp);
-			s.name = "";
-			sp = getval(sp);
-			pc = getval(sp);
-		}
-		if(pc == 0) {
-			print("//didn't find pc at sp=%#llux, last pc found at sp=%#llux\n", sp, osp);
-			break;
-		}
-		osp = sp;
-
-		if(!isintrr)
-			sp += mach->szaddr;
 
 		if(++i > 40)
 			break;
