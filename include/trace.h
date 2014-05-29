b20a5161972ed243f7058ce5152e8aaafc1e1e71
pad <pad@fb.com>
2014-05-29 12:07:32 -0700

    * include/trace.h: reindent

--- include/trace.h
+++ include/trace.h
@@ -1,34 +1,34 @@
 /*s: trace.h */
 /*s: enum Tevent */
 enum Tevent {
- SReady = 0,		/* runnable but not running  */
- SRun,		/* running best effort */
- SDead,		/* proc dies */
- SSleep,		/* blocked */
- SUser,		/* user event */
+    SReady = 0,		/* runnable but not running  */
+    SRun,		/* running best effort */
+    SDead,		/* proc dies */
+    SSleep,		/* blocked */
+    SUser,		/* user event */
 
-        /* real-time extensions */
-        /*s: [[enum Tevent]] real-time scheduling events */
-         SAdmit,	/* Edf admit */
-         SRelease,	/* Edf release, waiting to be scheduled */
-         SYield,		/* blocked waiting for release */
-         SSlice,		/* slice exhausted */
-         SDeadline,	/* proc's deadline */
-         SExpel,		/* Edf expel */
-         SInts,		/* Interrupt start */
-         SInte,		/* Interrupt end */
-        /*e: [[enum Tevent]] real-time scheduling events */
+    /* real-time extensions */
+    /*s: [[enum Tevent]] real-time scheduling events */
+    SAdmit,	/* Edf admit */
+    SRelease,	/* Edf release, waiting to be scheduled */
+    SYield,		/* blocked waiting for release */
+    SSlice,		/* slice exhausted */
+    SDeadline,	/* proc's deadline */
+    SExpel,		/* Edf expel */
+    SInts,		/* Interrupt start */
+    SInte,		/* Interrupt end */
+    /*e: [[enum Tevent]] real-time scheduling events */
 
- Nevent, // must be last
+    Nevent, // must be last
 };
 /*e: enum Tevent */
 
 /*s: struct Traceevent */
 struct Traceevent {
- ulong	pid;	
-        // enum<tevent>
- ulong	etype;	/* Event type */
- vlong	time;	/* time stamp  */ // dimention?
+    ulong	pid;	
+    // enum<tevent>
+    ulong	etype;	/* Event type */
+    vlong	time;	/* time stamp  */ // dimension?
 };
 /*e: struct Traceevent */
 
