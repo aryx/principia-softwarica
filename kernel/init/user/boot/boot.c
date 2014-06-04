d675b484246185a61c7f74bd73cdc8b97c07ae93
pad <pad@fb.com>
2014-06-04 11:47:18 -0700

    * kernel/init/user/boot/boot.c: misc

--- kernel/init/user/boot/boot.c
+++ kernel/init/user/boot/boot.c
@@ -66,7 +66,7 @@ execinit(void)
   //this just need a regular vga driver
   //run("/bin/vga", "-l", "640x480x8", nil);
   //this need special drivers, such as the clgd424x.c in the kernel
-  run("/bin/vga", "-l", "1024x768x8", nil);
+  run("/bin/vga", "-l", "1024x768x8", nil); // can add -V to debug vga
   bind_safe("#i", "/dev", MAFTER);
 
   // for rio
