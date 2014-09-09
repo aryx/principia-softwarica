330aa23c761f75cd13190c0499cf2918d3b6d54a
pad <pad@fb.com>
2014-09-09 00:14:08 -0700

    * compilers/cc/lexbody: removed, and inline some of the xxx to xxx.c

--- compilers/cc/compat.c
+++ compilers/cc/compat.c
@@ -1,6 +1,90 @@
 /*s: cc/compat.c */
 #include	"cc.h"
-#include	"compat"
+
+//#include	"compat"
+//TODO copy paste with aa/, maybe could factorize with another lib?
+//
+
+int
+myaccess(char *f)
+{
+	return access(f, AEXIST);
+}
+
+void*
+mysbrk(ulong size)
+{
+	return sbrk(size);
+}
+
+int
+mycreat(char *n, int p)
+{
+
+	return create(n, 1, p);
+}
+
+int
+mywait(int *s)
+{
+	int p;
+	Waitmsg *w;
+
+	if((w = wait()) == nil)
+		return -1;
+	else{
+		p = w->pid;
+		*s = 0;
+		if(w->msg[0])
+			*s = 1;
+		free(w);
+		return p;
+	}
+}
+
+int
+mydup(int f1, int f2)
+{
+	return dup(f1,f2);
+}
+
+int
+mypipe(int *fd)
+{
+	return pipe(fd);
+}
+
+int
+systemtype(int sys)
+{
+	return sys & Plan9;
+}
+
+int
+pathchar(void)
+{
+	return '/';
+}
+
+char*
+mygetwd(char *path, int len)
+{
+	return getwd(path, len);
+}
+
+int
+myexec(char *path, char *argv[])
+{
+	return exec(path, argv);
+}
+
+int
+myfork(void)
+{
+	return fork();
+}
+
+
 
 /*s: function malloc */
 /*
