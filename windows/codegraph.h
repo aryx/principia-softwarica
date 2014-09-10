cd433c9870955daf23c6a397cd92437bf83f9479
pad <pad@fb.com>
2014-09-10 15:38:46 -0700

    * windows/codegraph.h: unlpize

--- windows/codegraph.h
+++ windows/codegraph.h
@@ -1,18 +1,11 @@
-/*s: windows/codegraph.h */
-
 // in libc, malloc.c
+
 //Pool *mainmem = &sbrkmem;
-/*s: global imagmem */
 //Pool *imagmem = &sbrkmem;
 Pool*   imagmem = nil;
-/*e: global imagmem */
-
-
 // for kernel stuff:
 // globals: screenputs, vgacur, vgadev, 
 // types: Pcidev
 // functions: addphysseg(), mouseresize()
 
 // and of course panic(), outb(), devxxx(), up, ...
-
-/*e: windows/codegraph.h */
