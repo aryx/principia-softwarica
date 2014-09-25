2381a56fb7caf2ff3eb9d7ef7e1e1d5be50f5e95
pad <pad@fb.com>
2014-09-25 15:18:53 -0700

    * install_macos.txt:

--- env.sh
+++ env.sh
@@ -1,9 +1,15 @@
-export KENCC=/home/pad/kencc
-# remember to modify plan9/src/cmd/mk/shell.c and put rcshell as 
-# the default shell (the mkfiles in plan9 assumes rc)
-export PLAN9=/home/pad/Downloads/plan9port
+# try to use absolute paths below if you use a wrapper script 'mk'
 
-export PATH=$PLAN9/bin:$KENCC/bin:$PATH
+# for 8c, 8a, 8l, iar, etc
+export KENCC=/home/pad/kencc
+# for rc, mk, etc
+# (remember to modify plan9/src/cmd/mk/shell.c and put rcshell as 
+# the default shell (the mkfiles in plan9 assumes rc))
+export PLAN9=/home/pad/Downloads/plan9port
+# for 'ar' that will call kencc 'iar'
+export CROSS=/home/pad/plan9/CROSS
+
+export PATH=$CROSS/bin:$PLAN9/bin:$KENCC/bin:$PATH
 
 #for 8._cp to be found and called
 PATH=$PATH:.
