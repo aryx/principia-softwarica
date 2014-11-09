70deb8fb4527f46eeb5dd8bad005029206ccc4a4
pad <pad@fb.com>
2014-11-08 19:08:52 -0800

    * include/a.out.h: remove some archi X_MAGIC and some dependent code

--- include/a.out.h
+++ include/a.out.h
@@ -30,57 +30,15 @@ typedef	struct	Exec	Exec;
 #define	_MAGIC(f, b)	((f)|((((4*(b))+0)*(b))+7))
 /*e: function _MAGIC */
 
-/*s: constant A_MAGIC */
-#define	A_MAGIC		_MAGIC(0, 8)		/* 68020 (retired) */
-/*e: constant A_MAGIC */
 /*s: constant I_MAGIC */
 #define	I_MAGIC		_MAGIC(0, 11)		/* intel 386 */
 /*e: constant I_MAGIC */
-/*s: constant J_MAGIC */
-#define	J_MAGIC		_MAGIC(0, 12)		/* intel 960 (retired) */
-/*e: constant J_MAGIC */
-/*s: constant K_MAGIC */
-#define	K_MAGIC		_MAGIC(0, 13)		/* sparc */
-/*e: constant K_MAGIC */
-/*s: constant V_MAGIC */
-#define	V_MAGIC		_MAGIC(0, 16)		/* mips 3000 BE */
-/*e: constant V_MAGIC */
-/*s: constant X_MAGIC */
-#define	X_MAGIC		_MAGIC(0, 17)		/* att dsp 3210 (retired) */
-/*e: constant X_MAGIC */
-/*s: constant M_MAGIC */
-#define	M_MAGIC		_MAGIC(0, 18)		/* mips 4000 BE */
-/*e: constant M_MAGIC */
-/*s: constant D_MAGIC */
-#define	D_MAGIC		_MAGIC(0, 19)		/* amd 29000 (retired) */
-/*e: constant D_MAGIC */
 /*s: constant E_MAGIC */
 #define	E_MAGIC		_MAGIC(0, 20)		/* arm */
 /*e: constant E_MAGIC */
-/*s: constant Q_MAGIC */
-#define	Q_MAGIC		_MAGIC(0, 21)		/* powerpc */
-/*e: constant Q_MAGIC */
-/*s: constant N_MAGIC */
-#define	N_MAGIC		_MAGIC(0, 22)		/* mips 4000 LE */
-/*e: constant N_MAGIC */
-/*s: constant L_MAGIC */
-#define	L_MAGIC		_MAGIC(0, 23)		/* dec alpha (retired) */
-/*e: constant L_MAGIC */
-/*s: constant P_MAGIC */
-#define	P_MAGIC		_MAGIC(0, 24)		/* mips 3000 LE */
-/*e: constant P_MAGIC */
-/*s: constant U_MAGIC */
-#define	U_MAGIC		_MAGIC(0, 25)		/* sparc64 (retired) */
-/*e: constant U_MAGIC */
 /*s: constant S_MAGIC */
 #define	S_MAGIC		_MAGIC(HDR_MAGIC, 26)	/* amd64 */
 /*e: constant S_MAGIC */
-/*s: constant T_MAGIC */
-#define	T_MAGIC		_MAGIC(HDR_MAGIC, 27)	/* powerpc64 */
-/*e: constant T_MAGIC */
-/*s: constant R_MAGIC */
-#define	R_MAGIC		_MAGIC(HDR_MAGIC, 28)	/* arm64 */
-/*e: constant R_MAGIC */
 
 /*s: constant MIN_MAGIC */
 #define	MIN_MAGIC	8
--- linkers/libmach/executable.c
+++ linkers/libmach/executable.c
@@ -96,29 +96,6 @@ ExecTable exectab[] =
 Mach	*mach = &mi386;			/* Global current machine table */
 /*e: global mach */
 
-/*s: function couldbe4k */
-static ExecTable*
-couldbe4k(ExecTable *mp)
-{
-    Dir *d;
-    ExecTable *f;
-
-    if((d=dirstat("/proc/1/regs")) == nil)
-        return mp;
-    if(d->length < 32*8){		/* R3000 */
-        free(d);
-        return mp;
-    }
-    free(d);
-    for (f = exectab; f->magic; f++)
-        if(f->magic == M_MAGIC) {
-            f->name = "mips plan 9 executable on mips2 kernel";
-            return f;
-        }
-    return mp;
-}
-/*e: function couldbe4k */
-
 /*s: function crackhdr */
 int
 crackhdr(int fd, Fhdr *fp)
@@ -153,8 +130,8 @@ crackhdr(int fd, Fhdr *fp)
             if(mp->magic != (magic & ~DYN_MAGIC))
                 continue;
 
-            if(mp->magic == V_MAGIC)
-                mp = couldbe4k(mp);
+//            if(mp->magic == V_MAGIC)
+//                mp = couldbe4k(mp);
 
             if ((magic & DYN_MAGIC) && mp->dlmname != nil)
                 fp->name = mp->dlmname;
