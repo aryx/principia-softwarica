629a197b228113865f6af1c1b3efca176b6b9743
pad <pad@fb.com>
2014-11-26 19:30:31 -0800

    * lib_graphics/Graphics.nw: misc

--- kernel/devices/screen/screen.h
+++ kernel/devices/screen/screen.h
@@ -1,18 +1,16 @@
 /*s: kernel/devices/screen/screen.h */
 // this used to contain the stuff now in vga.h but those prototypes are
-// VGA independent, so better to have a generic screen.h interface.
+// VGA independent, so better to have a generic screen.h interface and vga.h
+// in a separate file.
 
-// forward decls
-typedef struct Cursorinfo Cursorinfo;
-
-// screen.c
+// defined in screen.c, set in xxxscreen.c, used by generic screen.c stuff
 extern Memimage *gscreen;
 extern Memdata gscreendata;
 extern Rectangle physgscreenr;  /* actual monitor size */
 
 /* defined in xxxscreen.c, needed by devdraw.c */
 extern void 	flushmemscreen(Rectangle);
-extern uchar* 	attachscreen(Rectangle*, ulong*, int*, int*, int*);
+extern byte* 	attachscreen(Rectangle*, ulong*, int*, int*, int*);
 extern void 	blankscreen(bool);
 extern void		getcolor(ulong, ulong*, ulong*, ulong*);
 extern int		setcolor(ulong, ulong, ulong, ulong);
@@ -27,6 +25,7 @@ struct Cursorinfo {
   Lock;
 };
 /*e: struct Cursorinfo */
+typedef struct Cursorinfo Cursorinfo;
 
 /* defined in devmouse.c, needed by ?? */ // just enough Mouse getters/setters
 extern Point 	mousexy(void);
