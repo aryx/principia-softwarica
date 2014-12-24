3b84a3f7d565930a901e9d3af36eecd4fe270427
pad <pad@fb.com>
2014-12-24 18:50:08 +0100

    * assemblers/5a/tests/helloa.s:

--- assemblers/5a/tests/helloa.s
+++ assemblers/5a/tests/helloa.s
@@ -2,14 +2,8 @@
 #include "/sys/src/libc/9syscall/sys.h"        
         
 TEXT _main(SB), $-4
-
-        MOVW $NOP, R0
-        MOVW $NOP, R0
-        MOVW $NOP, R0
         MOVW $NOP, R0
         MOVW $3, R1
         SWI
-loop:
-        B loop
-                
-        
+        MOVW $EXITS, R0
+        SWI
