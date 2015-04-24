feae058e87f5e97a12ee3c87a93970a324cc6536
pad <pad@fb.com>
2015-04-24 12:39:07 -0700

    * ape/lib/ap/plan9/brk.c: fix for the _BRK_ vs _BRK change

--- ape/lib/ap/plan9/brk.c
+++ ape/lib/ap/plan9/brk.c
@@ -4,30 +4,33 @@
 
 char	end[];
 static	char	*bloc = { end };
-extern	int	_BRK_(void*);
+//old: was _BRK_ but I ve replace brk_ by brk in libc
+extern	int	_BRK(void*);
 
-char *
-brk(char *p)
-{
-	unsigned long n;
-
-	n = (unsigned long)p;
-	n += 3;
-	n &= ~3;
-	if(_BRK_((void*)n) < 0){
-		errno = ENOMEM;
-		return (char *)-1;
-	}
-	bloc = (char *)n;
-	return 0;
-}
+// would interfere with my new brk syscall
+// and anyway was not used I think
+//char *
+//brk(char *p)
+//{
+//	unsigned long n;
+//
+//	n = (unsigned long)p;
+//	n += 3;
+//	n &= ~3;
+//	if(_BRK_((void*)n) < 0){
+//		errno = ENOMEM;
+//		return (char *)-1;
+//	}
+//	bloc = (char *)n;
+//	return 0;
+//}
 
 void *
 sbrk(unsigned long n)
 {
 	n += 3;
 	n &= ~3;
-	if(_BRK_((void *)(bloc+n)) < 0){
+	if(_BRK((void *)(bloc+n)) < 0){
 		errno = ENOMEM;
 		return (void *)-1;
 	}
