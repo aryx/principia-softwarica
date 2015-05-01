daa3a6bdf579dec616529486ec133ad3e94bca35
pad <pad@fb.com>
2015-04-30 19:52:17 -0700

    * env.sh: comment

--- env.sh
+++ env.sh
@@ -1,13 +1,18 @@
-# try to use absolute paths below if you use a wrapper script 'mk'
+# try to use absolute paths below if you use a wrapper script like my 'mk'
 
-# for 8c, 8a, 8l, iar, etc
+# for 8c, 8a, 8l, iar, etc 
 export KENCC=/home/pad/kencc
-# for rc, mk, etc
-# (remember to modify plan9/src/cmd/mk/shell.c and put rcshell as 
-# the default shell (the mkfiles in plan9 assumes rc))
+# for rc, mk (configured with rc), etc
 export PLAN9=/home/pad/Downloads/plan9port
 # for 'ar' that will call kencc 'iar'
 export CROSS=/home/pad/plan9/CROSS
+
+# Note that kencc has an 'mk' but it's configured with 'sh', 
+# so it's overriden by the 'mk' in plan9port via the PATH
+# ordering below. The reason to configure 'mk' with rc is
+# because the mkfiles in plan9 assumes rc in too many places.
+# So after downloading plan9port don't forget to modify
+# plan9/src/cmd/mk/shell.c and put rcshell as the default shell.
 
 export PATH=$CROSS/bin:$PLAN9/bin:$KENCC/bin:$PATH
 
