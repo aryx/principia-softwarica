b1b7df746572c9f57915436d1eac13953b843712
pad <pad@fb.com>
2015-05-05 18:17:34 -0700

    * lib_core/libc/port/cleanname.c: boolify

--- lib_core/libc/port/cleanname.c
+++ lib_core/libc/port/cleanname.c
@@ -12,24 +12,24 @@ cleanname(char *name)
 	char *d;	/* destination of copy */
 	char *d0;	/* start of path afer the root name */
 	Rune r;
-	int rooted;
+	bool rooted;
 
-	if(name[0] == 0)
+	if(name[0] == '\0')
 		return strcpy(name, ".");
-	rooted = 0;
+	rooted = false;
 	d0 = name;
 	if(d0[0] == '#'){
-		if(d0[1] == 0)
+		if(d0[1] == '\0')
 			return d0;
 		d0  += 1 + chartorune(&r, d0+1); /* ignore slash: #/ */
 		while(!SEP(*d0))
 			d0 += chartorune(&r, d0);
-		if(d0 == 0)
+		if(d0 == '\0')
 			return name;
 		d0++;	/* keep / after #<name> */
-		rooted = 1;
+		rooted = true;
 	}else if(d0[0] == '/'){
-		rooted = 1;
+		rooted = true;
 		d0++;
 	}
 
@@ -40,19 +40,19 @@ cleanname(char *name)
 			;
 	}
 	/* remove dup slashes */
-	for(d = d0; *s != 0; s++){
+	for(d = d0; *s != '\0'; s++){
 		*d++ = *s;
 		if(*s == '/')
 			while(s[1] == '/')
 				s++;
 	}
-	*d = 0;
+	*d = '\0';
 
 	d = d0;
 	s = d0;
-	while(*s != 0){
+	while(*s != '\0'){
 		if(s[0] == '.' && SEP(s[1])){
-			if(s[1] == 0)
+			if(s[1] == '\0')
 				break;
 			s+= 2;
 			continue;
@@ -61,7 +61,7 @@ cleanname(char *name)
 			if(d == d0){
 				if(rooted){
 					/* /../x -> /x */
-					if(s[2] == 0)
+					if(s[2] == '\0')
 						break;
 					s += 3;
 					continue;
@@ -75,7 +75,7 @@ cleanname(char *name)
 				assert(d-2 >= d0 && d[-1] == '/');
 				for(d -= 2; d > d0 && d[-1] != '/'; d--)
 						;
-				if(s[2] == 0)
+				if(s[2] == '\0')
 					break;
 				s += 3;
 				continue;
@@ -83,15 +83,15 @@ cleanname(char *name)
 		}
 		while(!SEP(*s))
 			*d++ = *s++;
-		if(*s == 0)
+		if(*s == '\0')
 			break;
 		
 		*d++ = *s++;
 	}
-	*d = 0;
+	*d = '\0';
 	if(d-1 > name && d[-1] == '/')	/* thanks to #/ */
-		*--d = 0;
-	if(name[0] == 0)
+		*--d = '\0';
+	if(name[0] == '\0')
 		strcpy(name, ".");
 	return name;
 }
