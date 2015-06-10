865e31f6cc14e89227a7e1daec3aef42ab05ba27
pad <yoann.padioleau@gmail.com>
2015-06-10 11:30:54 -0700

    * kernel/network/386/ether2000.c: lpized

--- kernel/network/386/ether2000.c
+++ kernel/network/386/ether2000.c
@@ -1,3 +1,4 @@
+/*s: kernel/network/386/ether2000.c */
 #include "u.h"
 #include "../port/lib.h"
 #include "mem.h"
@@ -10,6 +11,7 @@
 #include "etherif.h"
 #include "ether8390.h"
 
+/*s: enum _anon_ (kernel/network/386/ether2000.c) */
 /*
  * Driver written for the 'Notebook Computer Ethernet LAN Adapter',
  * a plug-in to the bus-slot on the rear of the Gateway NOMAD 425DXL
@@ -22,215 +24,233 @@
  * and a reset port.
  */
 enum {
-	Data		= 0x10,		/* offset from I/O base of data port */
-	Reset		= 0x1F,		/* offset from I/O base of reset port */
+    Data		= 0x10,		/* offset from I/O base of data port */
+    Reset		= 0x1F,		/* offset from I/O base of reset port */
 };
+/*e: enum _anon_ (kernel/network/386/ether2000.c) */
 
 typedef struct Ctlr Ctlr;
+/*s: struct Ctlr */
 typedef struct Ctlr {
-	Pcidev*	pcidev;
-	Ctlr*	next;
-	int	active;
+    Pcidev*	pcidev;
+    Ctlr*	next;
+    int	active;
 } Ctlr;
+/*e: struct Ctlr */
 
+/*s: global ctlrhead */
 static Ctlr* ctlrhead;
+/*e: global ctlrhead */
+/*s: global ctlrtail */
 static Ctlr* ctlrtail;
+/*e: global ctlrtail */
 
+/*s: global ne2000pci */
 static struct {
-	char*	name;
-	int	id;
+    char*	name;
+    int	id;
 } ne2000pci[] = {
-	{ "Realtek 8029",	(0x8029<<16)|0x10EC, },
-	{ "Winbond 89C940",	(0x0940<<16)|0x1050, },
-	{ nil },
+    { "Realtek 8029",	(0x8029<<16)|0x10EC, },
+    { "Winbond 89C940",	(0x0940<<16)|0x1050, },
+    { nil },
 };
+/*e: global ne2000pci */
 
+/*s: function ne2000match */
 static Ctlr*
 ne2000match(Ether* edev, int id)
 {
-	int port;
-	Pcidev *p;
-	Ctlr *ctlr;
+    int port;
+    Pcidev *p;
+    Ctlr *ctlr;
 
-	/*
-	 * Any adapter matches if no edev->port is supplied,
-	 * otherwise the ports must match.
-	 */
-	for(ctlr = ctlrhead; ctlr != nil; ctlr = ctlr->next){
-		if(ctlr->active)
-			continue;
-		p = ctlr->pcidev;
-		if(((p->did<<16)|p->vid) != id)
-			continue;
-		port = p->mem[0].bar & ~0x01;
-		if(edev->port != 0 && edev->port != port)
-			continue;
+    /*
+     * Any adapter matches if no edev->port is supplied,
+     * otherwise the ports must match.
+     */
+    for(ctlr = ctlrhead; ctlr != nil; ctlr = ctlr->next){
+        if(ctlr->active)
+            continue;
+        p = ctlr->pcidev;
+        if(((p->did<<16)|p->vid) != id)
+            continue;
+        port = p->mem[0].bar & ~0x01;
+        if(edev->port != 0 && edev->port != port)
+            continue;
 
-		/*
-		 * It suffices to fill these in,
-		 * the rest is gleaned from the card.
-		 */
-		edev->port = port;
-		edev->irq = p->intl;
+        /*
+         * It suffices to fill these in,
+         * the rest is gleaned from the card.
+         */
+        edev->port = port;
+        edev->irq = p->intl;
 
-		ctlr->active = 1;
+        ctlr->active = 1;
 
-		return ctlr;
-	}
+        return ctlr;
+    }
 
-	return nil;
+    return nil;
 }
+/*e: function ne2000match */
 
+/*s: function ne2000pnp */
 static void
 ne2000pnp(Ether* edev)
 {
-	int i, id;
-	Pcidev *p;
-	Ctlr *ctlr;
+    int i, id;
+    Pcidev *p;
+    Ctlr *ctlr;
 
-	/*
-	 * Make a list of all ethernet controllers
-	 * if not already done.
-	 */
-	if(ctlrhead == nil){
-		p = nil;
-		while(p = pcimatch(p, 0, 0)){
-			if(p->ccrb != 0x02 || p->ccru != 0)
-				continue;
-			ctlr = malloc(sizeof(Ctlr));
-			if(ctlr == nil)
-				error(Enomem);
-			ctlr->pcidev = p;
+    /*
+     * Make a list of all ethernet controllers
+     * if not already done.
+     */
+    if(ctlrhead == nil){
+        p = nil;
+        while(p = pcimatch(p, 0, 0)){
+            if(p->ccrb != 0x02 || p->ccru != 0)
+                continue;
+            ctlr = malloc(sizeof(Ctlr));
+            if(ctlr == nil)
+                error(Enomem);
+            ctlr->pcidev = p;
 
-			if(ctlrhead != nil)
-				ctlrtail->next = ctlr;
-			else
-				ctlrhead = ctlr;
-			ctlrtail = ctlr;
-		}
-	}
+            if(ctlrhead != nil)
+                ctlrtail->next = ctlr;
+            else
+                ctlrhead = ctlr;
+            ctlrtail = ctlr;
+        }
+    }
 
-	/*
-	 * Is it a card with an unrecognised vid+did?
-	 * Normally a search is made through all the found controllers
-	 * for one which matches any of the known vid+did pairs.
-	 * If a vid+did pair is specified a search is made for that
-	 * specific controller only.
-	 */
-	id = 0;
-	for(i = 0; i < edev->nopt; i++){
-		if(cistrncmp(edev->opt[i], "id=", 3) == 0)
-			id = strtol(&edev->opt[i][3], nil, 0);
-	}
+    /*
+     * Is it a card with an unrecognised vid+did?
+     * Normally a search is made through all the found controllers
+     * for one which matches any of the known vid+did pairs.
+     * If a vid+did pair is specified a search is made for that
+     * specific controller only.
+     */
+    id = 0;
+    for(i = 0; i < edev->nopt; i++){
+        if(cistrncmp(edev->opt[i], "id=", 3) == 0)
+            id = strtol(&edev->opt[i][3], nil, 0);
+    }
 
-	if(id != 0)
-		ne2000match(edev, id);
-	else for(i = 0; ne2000pci[i].name; i++){
-		if(ne2000match(edev, ne2000pci[i].id) != nil)
-			break;
-	}
+    if(id != 0)
+        ne2000match(edev, id);
+    else for(i = 0; ne2000pci[i].name; i++){
+        if(ne2000match(edev, ne2000pci[i].id) != nil)
+            break;
+    }
 }
+/*e: function ne2000pnp */
 
+/*s: function ne2000reset */
 static int
 ne2000reset(Ether* edev)
 {
-	ushort buf[16];
-	ulong port;
-	Dp8390 *dp8390;
-	int i;
-	uchar ea[Eaddrlen];
+    ushort buf[16];
+    ulong port;
+    Dp8390 *dp8390;
+    int i;
+    uchar ea[Eaddrlen];
 
-	if(edev->port == 0)
-		ne2000pnp(edev);
+    if(edev->port == 0)
+        ne2000pnp(edev);
 
-	/*
-	 * Set up the software configuration.
-	 * Use defaults for irq, mem and size
-	 * if not specified.
-	 * Must have a port, no more default.
-	 */
-	if(edev->port == 0)
-		return -1;
-	if(edev->irq == 0)
-		edev->irq = 2;
-	if(edev->mem == 0)
-		edev->mem = 0x4000;
-	if(edev->size == 0)
-		edev->size = 16*1024;
-	port = edev->port;
+    /*
+     * Set up the software configuration.
+     * Use defaults for irq, mem and size
+     * if not specified.
+     * Must have a port, no more default.
+     */
+    if(edev->port == 0)
+        return -1;
+    if(edev->irq == 0)
+        edev->irq = 2;
+    if(edev->mem == 0)
+        edev->mem = 0x4000;
+    if(edev->size == 0)
+        edev->size = 16*1024;
+    port = edev->port;
 
-	if(ioalloc(edev->port, 0x20, 0, "ne2000") < 0)
-		return -1;
+    if(ioalloc(edev->port, 0x20, 0, "ne2000") < 0)
+        return -1;
 
-	edev->ctlr = malloc(sizeof(Dp8390));
-	dp8390 = edev->ctlr;
-	if(dp8390 == nil)
-		error(Enomem);
-	dp8390->width = 2;
-	dp8390->ram = 0;
+    edev->ctlr = malloc(sizeof(Dp8390));
+    dp8390 = edev->ctlr;
+    if(dp8390 == nil)
+        error(Enomem);
+    dp8390->width = 2;
+    dp8390->ram = 0;
 
-	dp8390->port = port;
-	dp8390->data = port+Data;
+    dp8390->port = port;
+    dp8390->data = port+Data;
 
-	dp8390->tstart = HOWMANY(edev->mem, Dp8390BufSz);
-	dp8390->pstart = dp8390->tstart + HOWMANY(sizeof(Etherpkt), Dp8390BufSz);
-	dp8390->pstop = dp8390->tstart + HOWMANY(edev->size, Dp8390BufSz);
+    dp8390->tstart = HOWMANY(edev->mem, Dp8390BufSz);
+    dp8390->pstart = dp8390->tstart + HOWMANY(sizeof(Etherpkt), Dp8390BufSz);
+    dp8390->pstop = dp8390->tstart + HOWMANY(edev->size, Dp8390BufSz);
 
-	dp8390->dummyrr = 1;
-	for(i = 0; i < edev->nopt; i++){
-		if(strcmp(edev->opt[i], "nodummyrr"))
-			continue;
-		dp8390->dummyrr = 0;
-		break;
-	}
+    dp8390->dummyrr = 1;
+    for(i = 0; i < edev->nopt; i++){
+        if(strcmp(edev->opt[i], "nodummyrr"))
+            continue;
+        dp8390->dummyrr = 0;
+        break;
+    }
 
-	/*
-	 * Reset the board. This is done by doing a read
-	 * followed by a write to the Reset address.
-	 */
-	buf[0] = inb(port+Reset);
-	delay(2);
-	outb(port+Reset, buf[0]);
-	delay(2);
-	
-	/*
-	 * Init the (possible) chip, then use the (possible)
-	 * chip to read the (possible) PROM for ethernet address
-	 * and a marker byte.
-	 * Could just look at the DP8390 command register after
-	 * initialisation has been tried, but that wouldn't be
-	 * enough, there are other ethernet boards which could
-	 * match.
-	 * Parallels has buf[0x0E] == 0x00 whereas real hardware
-	 * usually has 0x57.
-	 */
-	dp8390reset(edev);
-	memset(buf, 0, sizeof(buf));
-	dp8390read(dp8390, buf, 0, sizeof(buf));
-	i = buf[0x0E] & 0xFF;
-	if((i != 0x00 && i != 0x57) || (buf[0x0F] & 0xFF) != 0x57){
-		iofree(edev->port);
-		free(edev->ctlr);
-		return -1;
-	}
+    /*
+     * Reset the board. This is done by doing a read
+     * followed by a write to the Reset address.
+     */
+    buf[0] = inb(port+Reset);
+    delay(2);
+    outb(port+Reset, buf[0]);
+    delay(2);
+    
+    /*
+     * Init the (possible) chip, then use the (possible)
+     * chip to read the (possible) PROM for ethernet address
+     * and a marker byte.
+     * Could just look at the DP8390 command register after
+     * initialisation has been tried, but that wouldn't be
+     * enough, there are other ethernet boards which could
+     * match.
+     * Parallels has buf[0x0E] == 0x00 whereas real hardware
+     * usually has 0x57.
+     */
+    dp8390reset(edev);
+    memset(buf, 0, sizeof(buf));
+    dp8390read(dp8390, buf, 0, sizeof(buf));
+    i = buf[0x0E] & 0xFF;
+    if((i != 0x00 && i != 0x57) || (buf[0x0F] & 0xFF) != 0x57){
+        iofree(edev->port);
+        free(edev->ctlr);
+        return -1;
+    }
 
-	/*
-	 * Stupid machine. Shorts were asked for,
-	 * shorts were delivered, although the PROM is a byte array.
-	 * Set the ethernet address.
-	 */
-	memset(ea, 0, Eaddrlen);
-	if(memcmp(ea, edev->ea, Eaddrlen) == 0){
-		for(i = 0; i < sizeof(edev->ea); i++)
-			edev->ea[i] = buf[i];
-	}
-	dp8390setea(edev);
+    /*
+     * Stupid machine. Shorts were asked for,
+     * shorts were delivered, although the PROM is a byte array.
+     * Set the ethernet address.
+     */
+    memset(ea, 0, Eaddrlen);
+    if(memcmp(ea, edev->ea, Eaddrlen) == 0){
+        for(i = 0; i < sizeof(edev->ea); i++)
+            edev->ea[i] = buf[i];
+    }
+    dp8390setea(edev);
 
-	return 0;
+    return 0;
 }
+/*e: function ne2000reset */
 
+/*s: function ether2000link */
 void
 ether2000link(void)
 {
-	addethercard("NE2000", ne2000reset);
+    addethercard("NE2000", ne2000reset);
 }
+/*e: function ether2000link */
+/*e: kernel/network/386/ether2000.c */
--- kernel/network/386/ether8390.h
+++ kernel/network/386/ether8390.h
@@ -1,74 +1,86 @@
+/*s: kernel/network/386/ether8390.h */
 /*
  * Ctlr for the boards using the National Semiconductor DP8390
  * and SMC 83C90 Network Interface Controller.
  * Common code is in ether8390.c.
  */
 typedef struct {
-	Lock;
+    Lock;
 
-	ulong	port;			/* I/O address of 8390 */
-	ulong	data;			/* I/O data port if no shared memory */
+    ulong	port;			/* I/O address of 8390 */
+    ulong	data;			/* I/O data port if no shared memory */
 
-	uchar	width;			/* data transfer width in bytes */
-	uchar	ram;			/* true if card has shared memory */
-	uchar	dummyrr;		/* do dummy remote read */
+    uchar	width;			/* data transfer width in bytes */
+    uchar	ram;			/* true if card has shared memory */
+    uchar	dummyrr;		/* do dummy remote read */
 
-	uchar	nxtpkt;			/* receive: software bndry */
-	uchar	pstart;
-	uchar	pstop;
+    uchar	nxtpkt;			/* receive: software bndry */
+    uchar	pstart;
+    uchar	pstop;
 
-	int	txbusy;			/* transmit */
-	uchar	tstart;			/* 8390 ring addresses */
+    int	txbusy;			/* transmit */
+    uchar	tstart;			/* 8390 ring addresses */
 
-	uchar	mar[8];			/* shadow multicast address registers */
-	int	mref[64];		/* reference counts for multicast groups */
+    uchar	mar[8];			/* shadow multicast address registers */
+    int	mref[64];		/* reference counts for multicast groups */
 } Dp8390;
 
+/*s: constant Dp8390BufSz */
 #define Dp8390BufSz	256
+/*e: constant Dp8390BufSz */
 
 extern int dp8390reset(Ether*);
 extern void *dp8390read(Dp8390*, void*, ulong, ulong);
 extern void dp8390getea(Ether*, uchar*);
 extern void dp8390setea(Ether*);
 
+/*s: macro regr */
 /*
  * x86-specific code.
  */
 #define regr(c, r)	inb((c)->port+(r))
+/*e: macro regr */
+/*s: macro regw */
 #define regw(c, r, v)	outb((c)->port+(r), (v))
+/*e: macro regw */
 
+/*s: function rdread */
 static void
 rdread(Dp8390* ctlr, void* to, int len)
 {
-	switch(ctlr->width){
-	default:
-		panic("dp8390 rdread: width %d\n", ctlr->width);
-		break;
+    switch(ctlr->width){
+    default:
+        panic("dp8390 rdread: width %d\n", ctlr->width);
+        break;
 
-	case 2:
-		inss(ctlr->data, to, len/2);
-		break;
+    case 2:
+        inss(ctlr->data, to, len/2);
+        break;
 
-	case 1:
-		insb(ctlr->data, to, len);
-		break;
-	}
+    case 1:
+        insb(ctlr->data, to, len);
+        break;
+    }
 }
+/*e: function rdread */
 
+/*s: function rdwrite */
 static void
 rdwrite(Dp8390* ctlr, void* from, int len)
 {
-	switch(ctlr->width){
-	default:
-		panic("dp8390 rdwrite: width %d\n", ctlr->width);
-		break;
+    switch(ctlr->width){
+    default:
+        panic("dp8390 rdwrite: width %d\n", ctlr->width);
+        break;
 
-	case 2:
-		outss(ctlr->data, from, len/2);
-		break;
+    case 2:
+        outss(ctlr->data, from, len/2);
+        break;
 
-	case 1:
-		outsb(ctlr->data, from, len);
-		break;
-	}
+    case 1:
+        outsb(ctlr->data, from, len);
+        break;
+    }
 }
+/*e: function rdwrite */
+/*e: kernel/network/386/ether8390.h */
