4a9dbd53748c70d90bb7f6dd77f9733d2facad86
pad <yoann.padioleau@gmail.com>
2015-10-23 11:36:08 -0700

    * assemblers/ocaml/object_code.ml:

--- assemblers/ocaml/object_code.ml
+++ assemblers/ocaml/object_code.ml
@@ -1,4 +1,6 @@
 
+type object_code = Ast_asm.program
+
 (* can normalize before? *)
 let save_obj prog file =
   Marshall.
--- assemblers/ocaml/object_code.mli
+++ assemblers/ocaml/object_code.mli
@@ -1,4 +1,4 @@
 
-val save_obj: Ast.program -> Common.filename -> unit
+type object_code = Ast_asm.program
 
-
+val save: object_code -> Common.filename -> unit
