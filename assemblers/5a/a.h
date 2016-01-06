de302ba55500b6c7973d7c534af19c6cc93d85f2
pad <yoann.padioleau@gmail.com>
2016-01-05 18:45:45 -0800

    * assemblers/5a/a.h: untabify

--- assemblers/5a/a.h
+++ assemblers/5a/a.h
@@ -3,7 +3,7 @@
 #include <5.out.h>
 
 /*s: constant FPCHIP(arm) */
-#define	FPCHIP		true
+#define FPCHIP      true
 /*e: constant FPCHIP(arm) */
 
 /*s: constant Always(arm) */
@@ -11,48 +11,48 @@
 /*e: constant Always(arm) */
 
 /*s: struct Gen(arm) */
-struct	Gen
+struct  Gen
 {
     // enum<Operand_kind>
-    short	type;
+    short   type;
 
     // switch on Gen.type
     union {
-        long	offset; // offset or lval or ...
-        double	dval;
-        char	sval[NSNAME];
+        long    offset; // offset or lval or ...
+        double  dval;
+        char    sval[NSNAME];
     };
     // option<enum<Register>> (None = R_NONE)
-    short	reg; // abused also to store a size for DATA
+    short   reg; // abused also to store a size for DATA
 
     /*s: [[Gen]] other fields */
     // option<ref<Sym>> (owner = hash)
-    Sym*	sym;
+    Sym*    sym;
     /*x: [[Gen]] other fields */
     // option<enum<Sym_kind>> (None = N_NONE)
-    short	symkind;
+    short   symkind;
     /*e: [[Gen]] other fields */
 };
 /*e: struct Gen(arm) */
-typedef	struct	Gen	Gen;
+typedef struct  Gen Gen;
 
 // globals.c
-extern	Gen	nullgen;
+extern  Gen nullgen;
 
 // lex.c (for y.tab.c, main.c)
 /*s: signature yylex */
 // unit -> (enum<token_code> | -1 (EOF) | Rune)
-long	yylex(void);
+long    yylex(void);
 /*e: signature yylex */
-void	cinit(void);
+void    cinit(void);
 
 // y.tab.c from a.y (for main.c)
-int	yyparse(void);
+int yyparse(void);
 
 // obj.c (for main.c)
 /*s: signature outcode(arm) */
-void	outcode(int opcode, int cond, Gen* opd1, int reg, Gen* opd3);
+void    outcode(int opcode, int cond, Gen* opd1, int reg, Gen* opd3);
 /*e: signature outcode(arm) */
-void	outhist(void);
+void    outhist(void);
 
 /*e: assemblers/5a/a.h */
--- assemblers/aa/aa.h
+++ assemblers/aa/aa.h
@@ -13,50 +13,50 @@
 
 #include <common.out.h>
 
-#pragma	lib	"../aa/aa.a$O"
+#pragma lib "../aa/aa.a$O"
 
-typedef	struct	Sym	Sym;
+typedef struct  Sym Sym;
 typedef struct  Io Io;
-typedef	struct	Hist Hist;
+typedef struct  Hist Hist;
 
 /*s: constant MAXALIGN */
 #define MAXALIGN 7
 /*e: constant MAXALIGN */
 /*s: constant NSYMB */
-#define	NSYMB		500
+#define NSYMB       500
 /*e: constant NSYMB */
 /*s: constant BUFSIZ */
-#define	BUFSIZ		8192
+#define BUFSIZ      8192
 /*e: constant BUFSIZ */
 /*s: constant HISTSZ */
-#define	HISTSZ		20
+#define HISTSZ      20
 /*e: constant HISTSZ */
 /*s: constant NINCLUDE */
-#define	NINCLUDE	10
+#define NINCLUDE    10
 /*e: constant NINCLUDE */
 /*s: constant NHUNK */
-#define	NHUNK		10000
+#define NHUNK       10000
 /*e: constant NHUNK */
 /*s: constant EOF */
-#define	EOF		(-1)
+#define EOF     (-1)
 /*e: constant EOF */
 /*s: constant IGN */
-#define	IGN		(-2)
+#define IGN     (-2)
 /*e: constant IGN */
 /*s: function GETC */
 /// main -> assemble -> yyparse -> yylex -> <>
-#define	GETC()		((--fi.c < 0) ? filbuf() : *fi.p++ & 0xff)
+#define GETC()      ((--fi.c < 0) ? filbuf() : *fi.p++ & 0xff)
 /*e: function GETC */
 /*s: constant NHASH */
-#define	NHASH		503
+#define NHASH       503
 /*e: constant NHASH */
 /*s: constant STRINGSZ */
-#define	STRINGSZ	200
+#define STRINGSZ    200
 /*e: constant STRINGSZ */
 
 
 /*s: struct Sym */
-struct	Sym
+struct  Sym
 {
     // Sym is (ab)used to represent many things in the assembler:
     //   actual symbols, labels, but also macros,
@@ -66,24 +66,24 @@ struct	Sym
     // The "key"
     // ---------------------------------------------------------
     // ref_own<string>
-    char	*name;
+    char    *name;
 
     // ---------------------------------------------------------
     // The generic "value"
     // ---------------------------------------------------------
-    long	value; 
+    long    value; 
 
     /*s: [[Sym]] identifier fields */
     // index in h when the Sym is a symbol, 0 otherwise
-    int	symidx;
+    int symidx;
     /*e: [[Sym]] identifier fields */
     /*s: [[Sym]] macro fields */
     //option<string>  for '#define FOO xxx' expansion
-    char*	macro;
+    char*   macro;
     /*e: [[Sym]] macro fields */
     /*s: [[Sym]] token fields */
     //enum<token_code> (e.g. LLAB, LNAME, LVAR, LARITH, etc)
-    ushort	type;
+    ushort  type;
     /*e: [[Sym]] token fields */
 
     // ---------------------------------------------------------
@@ -91,46 +91,46 @@ struct	Sym
     // ---------------------------------------------------------
     /*s: [[Sym]] extra fields */
     // list<ref<Sym>> (from = hash)
-    Sym*	link;
+    Sym*    link;
     /*e: [[Sym]] extra fields */
 };
 /*e: struct Sym */
 /*s: constant S */
-#define	S	((Sym*)nil)
+#define S   ((Sym*)nil)
 /*e: constant S */
 
 /*s: struct Fi */
 struct Fi
 {
     // ref<char> (target = Io.b)
-    char*	p;
+    char*   p;
     // remaining characters in Io.b to read
-    int		c;
+    int     c;
 };
 /*e: struct Fi */
 extern struct Fi fi;
 
 /*s: struct Io */
-struct	Io
+struct  Io
 {
     // option<fdt> (None = FD_NONE)
-    fdt	f;
+    fdt f;
     /*s: [[Io]] buffer fields */
-    char	b[BUFSIZ];
+    char    b[BUFSIZ];
     /*x: [[Io]] buffer fields */
     // like Fi, saved pointers in Io.b
-    char*	p;
-    short	c;
+    char*   p;
+    short   c;
     /*e: [[Io]] buffer fields */
     // Extra
     /*s: [[Io]] extra fields */
     // list<ref_own<Io>> (from = iostack or iofree)
-    Io*	link;
+    Io* link;
     /*e: [[Io]] extra fields */
 };
 /*e: struct Io */
 /*s: constant I */
-#define	I	((Io*)nil)
+#define I   ((Io*)nil)
 /*e: constant I */
 
 /*s: constant FD_NONE */
@@ -141,119 +141,119 @@ struct	Io
 struct Htab
 {
     // option<ref<Sym>>>
-    Sym*	sym;
+    Sym*    sym;
     //enum<Sym_kind>
-    short	symkind;
+    short   symkind;
 };
 /*e: struct Htab */
 extern struct Htab h[NSYM];
 
 /*s: struct Hist */
-struct	Hist
+struct  Hist
 {
-    char*	filename; // nil for a ``pop''
+    char*   filename; // nil for a ``pop''
 
     // global line of this Hist
-    long	global_line;       
+    long    global_line;       
     // 0 for #include, +n for #line, -1 for #pragma lib (ugly)
-    long	local_line; 
+    long    local_line; 
 
     // Extra
     /*s: [[Hist]] extra fields */
     // list<ref<Hist>> (from = hist)
-    Hist*	link;
+    Hist*   link;
     /*e: [[Hist]] extra fields */
 };
 /*e: struct Hist */
 /*s: constant H */
-#define	H	((Hist*)nil)
+#define H   ((Hist*)nil)
 /*e: constant H */
 
 // core algorithm
-extern	Sym*	hash[NHASH];
-extern	int	pass;
-extern	Biobuf	obuf;
-extern	char*	outfile;
-extern	char*	pathname;
-extern	long	pc;
-extern	int	symcounter;
-extern	int	thechar;
-extern	char*	thestring;
+extern  Sym*    hash[NHASH];
+extern  int pass;
+extern  Biobuf  obuf;
+extern  char*   outfile;
+extern  char*   pathname;
+extern  long    pc;
+extern  int symcounter;
+extern  int thechar;
+extern  char*   thestring;
 
 // input files (used by Ya/lex.c)
-extern	Io*	iostack;
-extern	Io*	iofree;
-extern	Io*	ionext;
+extern  Io* iostack;
+extern  Io* iofree;
+extern  Io* ionext;
 
 // cpp
-extern	char*	include[NINCLUDE];
-extern	int	ninclude;
-extern	char*	Dlist[30];
-extern	int	nDlist;
+extern  char*   include[NINCLUDE];
+extern  int ninclude;
+extern  char*   Dlist[30];
+extern  int nDlist;
 
 // lexer
-extern	char	symb[NSYMB];
-extern	int	peekc;
+extern  char    symb[NSYMB];
+extern  int peekc;
 
 // debugging support
-extern	Hist*	hist;
-extern	Hist*	ehist;
-extern	long	lineno;
+extern  Hist*   hist;
+extern  Hist*   ehist;
+extern  long    lineno;
 
 // debugging
-extern	bool	debug[256];
+extern  bool    debug[256];
 
 // error managments
-extern	int	nerrors;
+extern  int nerrors;
 
 // utils (used by mac.c)
-extern	char*	hunk;
-extern	long	nhunk;
-extern	long	thunk;
+extern  char*   hunk;
+extern  long    nhunk;
+extern  long    thunk;
 
 // compact.c
-int	systemtype(int);
-int	pathchar(void);
-int	mywait(int*);
-int	mycreat(char*, int);
+int systemtype(int);
+int pathchar(void);
+int mywait(int*);
+int mycreat(char*, int);
 
 // utils.c
 void* alloc(long n);
 void* allocn(void *p, long on, long n);
-void	gethunk(void);
+void  gethunk(void);
 
 // lookup.c
-Sym*	slookup(char*);
-Sym*	lookup(void);
+Sym*    slookup(char*);
+Sym*    lookup(void);
 // this actually must be defined in Ya/lex.c, depends on LNAME
-void	syminit(Sym*);
+void    syminit(Sym*);
 
 // lexbody.c (used by Ya/lex.c)
-int	escchar(int);
+int escchar(int);
 
 
 
 // for lexbody
-void	setinclude(char*);
-void	pinit(char*);
-void	dodefine(char*);
-void	domacro(void);
-void	macund(void);
-void	macdef(void);
-void	macexpand(Sym*, char*);
-void	macinc(void);
-void	maclin(void);
-void	macprag(void);
-void	macif(int);
-void	macend(void);
-int	getnsc(void);
-void	yyerror(char*, ...);
-void	linehist(char*, int);
-int	filbuf(void);
+void    setinclude(char*);
+void    pinit(char*);
+void    dodefine(char*);
+void    domacro(void);
+void    macund(void);
+void    macdef(void);
+void    macexpand(Sym*, char*);
+void    macinc(void);
+void    maclin(void);
+void    macprag(void);
+void    macif(int);
+void    macend(void);
+int getnsc(void);
+void    yyerror(char*, ...);
+void    linehist(char*, int);
+int filbuf(void);
 
 // for macbody
-int	getc(void);
-void	unget(int);
+int getc(void);
+void    unget(int);
 
 // for macbody, was in lexbody
 void pragpack(void);
@@ -263,23 +263,23 @@ void pragvararg(void);
 void pragincomplete(void);
 
 
-void	pushio(void);
-void	newio(void);
-void	newfile(char*, int);
+void    pushio(void);
+void    newio(void);
+void    newfile(char*, int);
 
-void	errorexit(void);
+void    errorexit(void);
 
 // from lexbody.c
 void ieeedtod(Ieee *ieee, double native);
 
 /*s: enum _anon_ (assemblers/aa/aa.h) */
 /*
- *	system-dependent stuff from ../cc/compat.c
+ *  system-dependent stuff from ../cc/compat.c
  */
-enum	/* keep in synch with ../cc/cc.h */
+enum    /* keep in synch with ../cc/cc.h */
 {
-    Plan9	= 1<<0,
-    Unix	= 1<<1,
+    Plan9   = 1<<0,
+    Unix    = 1<<1,
 };
 /*e: enum _anon_ (assemblers/aa/aa.h) */
 /*e: assemblers/aa/aa.h */
