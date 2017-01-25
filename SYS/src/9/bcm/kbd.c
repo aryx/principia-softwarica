ec87a7bf106dd7bdae8e26bea796494ba336eb3d
pad <yoann.padioleau@gmail.com>
2017-01-25 13:56:43 -0800

    * SYS/src/9/bcm/kbd.c: comment

--- SYS/src/9/bcm/kbd.c
+++ SYS/src/9/bcm/kbd.c
@@ -5,8 +5,9 @@
 #include "dat.h"
 #include "fns.h"
 
-// In the Raspberry Pi the keyboard is handled by usbd
-// with help from /dev/kbin
+// In the Raspberry Pi, the keyboard is handled by usbd
+// with help from /dev/kbin (which then calls functions from portkbd
+// which calls arch_setleds below).
 
 // empty callback for portkbd.c
 void arch_setleds(Kbscan *kbscan) { USED(kbscan); }
