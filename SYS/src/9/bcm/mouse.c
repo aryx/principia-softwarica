e8889cebcb91c1ee8ec411e7b5d3f72d4609ff3f
pad <yoann.padioleau@gmail.com>
2017-01-25 14:01:01 -0800

    * kernel/devices/screen/arm/screen.c: mv

--- SYS/src/9/bcm/mouse.c
+++ SYS/src/9/bcm/mouse.c
@@ -1,18 +1,1 @@
-#include "u.h"
-#include "../port/lib.h"
-#include "../port/error.h"
-#include "mem.h"
-#include "dat.h"
-#include "fns.h"
-
-// In the Raspberry Pi, the mouse is handled by usbd
-// with help from /dev/mousein.
-
-// empty callbacks for portmouse.c
-
-void arch_ps2mouse(void) { }
-void arch_setaccelerated(void) { }
-void arch_setlinear(void) { }
-void arch_setres(int n) { USED(n); }
-void arch_setintellimouse(void) { }
-void arch_resetmouse(void) { }
+../../../../kernel/devices/mouse/arm/mouse.c
