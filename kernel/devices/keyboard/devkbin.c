ae6420c722673ab7cc1408cd86316ad4ebdc8b9c
pad <yoann.padioleau@gmail.com>
2017-03-04 10:50:21 -0800

    * kernel/devices/keyboard/devkbmap.c: untabify. Also put 'K' for device char for kbmap because was getting 5c error on unicode character

--- kernel/devices/keyboard/devkbin.c
+++ kernel/devices/keyboard/devkbin.c
@@ -4,118 +4,119 @@
  *  to avoid duplication of keyboard map processing for usb.
  */
 
-#include	"u.h"
-#include	"../port/lib.h"
-#include	"../port/error.h"
-#include	"mem.h"
-#include	"dat.h"
-#include	"fns.h"
+#include    "u.h"
+#include    "../port/lib.h"
+#include    "mem.h"
+#include    "dat.h"
+#include    "fns.h"
+#include    "../port/error.h"
+
+extern  void kbdputsc(byte, int);
 
 enum {
- Qdir,
- Qkbd,
+    Qdir,
+    Qkbd,
 };
 
 Dirtab kbintab[] = {
- ".",	{Qdir, 0, QTDIR},	0,	0555,
- "kbin",	{Qkbd, 0},		0,	0200,
+    ".",    {Qdir, 0, QTDIR},   0,  0555,
+    "kbin", {Qkbd, 0},      0,  0200,
 };
 
-Lock	kbinlck;
-int	kbinbusy;
+Lock    kbinlck;
+int kbinbusy;
 
 static Chan *
 kbinattach(char *spec)
 {
- return devattach(L'Ι', spec);
+    return devattach(L'Ι', spec);
 }
 
 static Walkqid*
 kbinwalk(Chan *c, Chan *nc, char **name, int nname)
 {
- return devwalk(c, nc, name, nname, kbintab, nelem(kbintab), devgen);
+    return devwalk(c, nc, name, nname, kbintab, nelem(kbintab), devgen);
 }
 
 static int
 kbinstat(Chan *c, uchar *dp, int n)
 {
- return devstat(c, dp, n, kbintab, nelem(kbintab), devgen);
+    return devstat(c, dp, n, kbintab, nelem(kbintab), devgen);
 }
 
 static Chan*
 kbinopen(Chan *c, int omode)
 {
- if(!iseve())
-  error(Eperm);
- if(c->qid.path == Qkbd){
-  lock(&kbinlck);
-  if(kbinbusy){
-   unlock(&kbinlck);
-   error(Einuse);
-  }
-  kbinbusy++;
-  unlock(&kbinlck);
- }
- return devopen(c, omode, kbintab, nelem(kbintab), devgen);
+    if(!iseve())
+        error(Eperm);
+    if(c->qid.path == Qkbd){
+        lock(&kbinlck);
+        if(kbinbusy){
+            unlock(&kbinlck);
+            error(Einuse);
+        }
+        kbinbusy++;
+        unlock(&kbinlck);
+    }
+    return devopen(c, omode, kbintab, nelem(kbintab), devgen);
 }
 
 static void
 kbinclose(Chan *c)
 {
- if(c->aux){
-  free(c->aux);
-  c->aux = nil;
- }
- if(c->qid.path == Qkbd)
-  kbinbusy = 0;
+    if(c->aux){
+        free(c->aux);
+        c->aux = nil;
+    }
+    if(c->qid.path == Qkbd)
+        kbinbusy = 0;
 }
 
 static long
 kbinread(Chan *c, void *a, long n, vlong )
 {
- if(c->qid.type == QTDIR)
-  return devdirread(c, a, n, kbintab, nelem(kbintab), devgen);
- return 0;
+    if(c->qid.type == QTDIR)
+        return devdirread(c, a, n, kbintab, nelem(kbintab), devgen);
+    return 0;
 }
 
 static long
 kbinwrite(Chan *c, void *a, long n, vlong)
 {
- int i;
- uchar *p = a;
+    int i;
+    uchar *p = a;
 
- if(c->qid.type == QTDIR)
-  error(Eisdir);
- switch((int)c->qid.path){
- case Qkbd:
-  for(i = 0; i < n; i++)
-            // !! important call !!
-   kbdputsc(*p++, 1);	/* external source */
-  break;
- default:
-  error(Egreg);
- }
- return n;
+    if(c->qid.type == QTDIR)
+        error(Eisdir);
+    switch((int)c->qid.path){
+    case Qkbd:
+        for(i = 0; i < n; i++)
+            kbdputsc(*p++, 1);  /* external source */
+        break;
+    default:
+        error(Egreg);
+    }
+    return n;
 }
 
 Dev kbindevtab = {
- .dc = L'Ι',
- .name = "kbin",
-
- .reset = devreset,
- .init = devinit,
- .shutdown = devshutdown,
- .attach = kbinattach,
- .walk = kbinwalk,
- .stat = kbinstat,
- .open = kbinopen,
- .create = devcreate,
- .close = kbinclose,
- .read = kbinread,
- .bread = devbread,
- .write = kbinwrite,
- .bwrite = devbwrite,
- .remove = devremove,
- .wstat = devwstat,
+    .dc = L'Ι',
+    .name = "kbin",
+   
+    .reset = devreset,
+    .init = devinit,
+    .shutdown = devshutdown,
+    .attach = kbinattach,
+    .walk = kbinwalk,
+    .stat = kbinstat,
+    .open = kbinopen,
+    .create = devcreate,
+    .close = kbinclose,
+    .read = kbinread,
+    .bread = devbread,
+    .write = kbinwrite,
+    .bwrite = devbwrite,
+    .remove = devremove,
+    .wstat = devwstat,
 };
 /*e: devkbin.c */
