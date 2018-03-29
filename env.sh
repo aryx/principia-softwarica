f72d97cc4fe1c05309ec1b9cff14afbfabda49c0
pad <yoann.padioleau@gmail.com>
2018-03-28 19:43:57 -0700

    * env.sh: starting to remove the ugly need for /386 and /sys

--- env.sh
+++ env.sh
@@ -1,24 +1,28 @@
 # try to use absolute paths below if you use a wrapper script like my 'mk'
 
+export objtype=386
+export cputype=386
+#alt: export objtype=arm
+
+export ROOT=/home/pad/plan9/ROOT
+
+
 # for 8c, 8a, 8l, iar, etc 
 export KENCC=/home/pad/kencc
 # for rc, mk (configured with rc), etc
-export PLAN9=/home/pad/Downloads/plan9port
+export PLAN9=/home/pad/packages/stow/plan9port
 # for 'ar' that will call kencc 'iar'
 export CROSS=/home/pad/plan9/CROSS
 
-# Note that kencc has an 'mk' but it's configured with 'sh', 
-# so it's overriden by the 'mk' in plan9port via the PATH
+# Note that kencc has an 'mk' but it is configured with 'sh',
+# so I override it by the 'mk' in plan9port via the PATH
 # ordering below. The reason to configure 'mk' with rc is
 # because the mkfiles in plan9 assumes rc in too many places.
-# So after downloading plan9port don't forget to modify
-# plan9/src/cmd/mk/shell.c and put rcshell as the default shell.
+# So after downloading plan9port, don't forget to modify
+# plan9port/src/cmd/mk/shell.c and put rcshell as the default shell.
 
 export PATH=$CROSS/bin:$PLAN9/bin:$KENCC/bin:$PATH
 
 #for 8._cp to be found and called
 PATH=$PATH:.
 
-export objtype=386
-export cputype=386
-#alt: export objtype=arm
