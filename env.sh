89b6fa00cad6b6cd793e37d255e06e62c6d254a4
pad <yoann.padioleau@gmail.com>
2025-06-19 17:20:11 +0200

    misc

--- env.sh
+++ env.sh
@@ -1,12 +1,16 @@
+# can use mk from xix or kencc but should not use
+# rc from xix nor kencc!!!
+
 # VARIABLE TO ADJUST
 # path to kencc toplevel directory
 KENCC=$HOME/kencc
 
 # DO NOT MODIFY THOSE VARIABLES
-# to find mk, 8c/5c, 8a/5a, 8l/5l, rc, sed, ...
+# to find mk, 8c/5c, 8a/5a, 8l/5l, sed, ... (but NOT rc right now!)
 export PATH=$KENCC/bin:$PATH
 # for mk to find and use rc
-#export MKSHELL=$KENCC/bin/rc
+#TODO: buggy! export MKSHELL=$KENCC/bin/rc
 export MKSHELL=`which rc`
 # for rc to find its rcmain initialization file
+# or hack 
 #export RCMAIN=$KENCC/lib/rcmain.unix
