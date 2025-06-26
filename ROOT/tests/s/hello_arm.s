15b7e521ae766d96fb16bf03e4e59665eb8f0606
pad <yoann.padioleau@gmail.com>
2025-06-26 16:56:49 +0200

    comment on weird bug in hello_arm.s

--- ROOT/tests/s/hello_arm.s
+++ ROOT/tests/s/hello_arm.s
@@ -1,4 +1,8 @@
 TEXT _main(SB), $20
+	// optional for this program as 5l managed to use R15 (PC) to
+        // compute $hello(SB) instead of using R12
+        // see also libc/arm/main9.s
+	MOVW	$setR12(SB), R12
         /* prepare the system call PWRITE(1,&hello,12, 00) */
         MOVW $1, R1
         MOVW R1, 4(R13)
@@ -13,10 +17,20 @@ TEXT _main(SB), $20
         /* system call */
         SWI $0
         BL exit(SB)
+//TODO: REALLY BAD BUG
+// if you comment the RET and B loop below, which mean
+// BL exit(SB) should just go the next instruction, then
+// weirdly some wrong code is generated and a BL 40001098 is generated
+// with a big Text address resulting in a fault.
+// Both kencc/5l and myplan9/5l seems to have this bug!!
+// What about goken/5l???
+//        RET /* not reached */
+//loop:
+//        B loop
 
 TEXT exit(SB), $4
-        /* prepare the system call EXITS("hello world") */
-        MOVW $hello(SB), R1
+        /* prepare the system call EXITS(0) */
+        MOVW $0, R1
         MOVW R1, 4(R13)
         MOVW $3 /*EXITS*/, R0
         /* system call */
