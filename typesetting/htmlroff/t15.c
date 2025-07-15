8c0b789855f90975061f98c182fbbe984133035d
pad <yoann.padioleau@gmail.com>
2025-07-15 11:11:55 +0200

    adding typesetting/htmlroff/

---
+++ typesetting/htmlroff/t15.c
@@ -0,0 +1,12 @@
+#include "a.h"
+
+/*
+ * 15. Output line numbering.
+ */
+
+void
+t15init(void)
+{
+	addreq(L("nm"), r_warn, -1);
+	addreq(L("nn"), r_warn, -1);
+}
---
+++ typesetting/htmlroff/t9.c
@@ -0,0 +1,5 @@
+/*
+ * 9.  Tabs, leaders, and fields.
+ */
+
+XXX
