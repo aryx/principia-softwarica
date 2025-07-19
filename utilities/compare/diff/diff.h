9c4d8e357628c089f319f321c01060f1d64cb544
pad <yoann.padioleau@gmail.com>
2025-07-19 15:39:16 +0200

    untabify diff

--- utilities/compare/diff/diff.h
+++ utilities/compare/diff/diff.h
@@ -1,16 +1,16 @@
-char mode;			/* '\0', 'e', 'f', 'h' */
-char bflag;			/* ignore multiple and trailing blanks */
-char rflag;			/* recurse down directory trees */
-char mflag;			/* pseudo flag: doing multiple files, one dir */
+char mode;          /* '\0', 'e', 'f', 'h' */
+char bflag;         /* ignore multiple and trailing blanks */
+char rflag;         /* recurse down directory trees */
+char mflag;         /* pseudo flag: doing multiple files, one dir */
 int anychange;
-extern Biobuf	stdout;
-extern int	binary;
+extern Biobuf   stdout;
+extern int  binary;
 
-#define MALLOC(t, n)		((t *)emalloc((n)*sizeof(t)))
-#define REALLOC(p, t, n)	((t *)erealloc((void *)(p), (n)*sizeof(t)))
-#define FREE(p)			free((void *)(p))
+#define MALLOC(t, n)        ((t *)emalloc((n)*sizeof(t)))
+#define REALLOC(p, t, n)    ((t *)erealloc((void *)(p), (n)*sizeof(t)))
+#define FREE(p)         free((void *)(p))
 
-#define MAXPATHLEN	1024
+#define MAXPATHLEN  1024
 
 int mkpathname(char *, char *, char *);
 void *emalloc(unsigned);
