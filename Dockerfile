c3e594a56269c2b30f360c543e2f99df82acc9a3
pad <yoann.padioleau@gmail.com>
2026-01-12 13:40:06 +0100

    switch to goken in Dockerfile!

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,16 @@
 # Build principia on Ubuntu Linux for 386 (pc) and arm (pi)
 
-FROM padator/kencc
+FROM padator/goken
 
-# 9base for rc (TODO: delete once we can have a working rc in kencc)
-RUN apt-get install -y 9base
-
-WORKDIR /src
+WORKDIR /principia
 
 # Now let's build from source
 COPY . .
 
 # 386
 RUN cp mkconfig.pc mkconfig
-RUN . ./env.sh && mk && mk install && mk kernel
+RUN mk && mk install && mk kernel
 
 # arm
 RUN cp mkconfig.pi mkconfig
-RUN . ./env.sh && mk && mk install && mk kernel
+RUN mk && mk install && mk kernel
--- env.sh
+++ env.sh
@@ -1,17 +1,27 @@
-# can use mk from xix or kencc or goken but should not use
-# rc from xix nor kencc!!!
+GOKEN=$HOME/goken
+# to find mk, rc, 8c/5c, 8a/5a, 8l/5l, sed, ...
+export PATH=$GOKEN/bin:$PATH
+#coupling: goken/env.sh
+export MKSHELL=$GOKEN/bin/rc
+export RCMAIN=$GOKEN/etc/rcmain.unix
+export YACCPAR=$GOKEN/etc/yaccpar
+#export NPROC=`nproc`
 
-# VARIABLE TO ADJUST
-# path to kencc toplevel directory
-KENCC=$HOME/kencc
-#KENCC=/home/pad/work/KENCC/9-cc-david-colombier/Linux/386
 
-# DO NOT MODIFY THOSE VARIABLES
-# to find mk, 8c/5c, 8a/5a, 8l/5l, sed, ... (but NOT rc right now!)
-export PATH=$KENCC/bin:$PATH
-# for mk to find and use rc
-#TODO: buggy! export MKSHELL=$KENCC/bin/rc
-export MKSHELL=`which rc`
-# for rc to find its rcmain initialization file
-# or hack 
-#export RCMAIN=$KENCC/lib/rcmain.unix
+#old: alt: when using kencc
+## can use mk from xix or kencc or goken but should not use
+## rc from xix nor kencc!!!
+## VARIABLE TO ADJUST
+## path to kencc toplevel directory
+#KENCC=$HOME/kencc
+##KENCC=/home/pad/work/KENCC/9-cc-david-colombier/Linux/386
+#
+## DO NOT MODIFY THOSE VARIABLES
+## to find mk, 8c/5c, 8a/5a, 8l/5l, sed, ... (but NOT rc right now!)
+#export PATH=$KENCC/bin:$PATH
+## for mk to find and use rc
+##TODO: buggy! export MKSHELL=$KENCC/bin/rc
+#export MKSHELL=`which rc`
+## for rc to find its rcmain initialization file
+## or hack 
+##export RCMAIN=$KENCC/lib/rcmain.unix
