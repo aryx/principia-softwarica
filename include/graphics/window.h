9b44b59f8b072362b9d5a8f2c453d43e7493bb64
pad <yoann.padioleau@gmail.com>
2026-02-12 10:15:26 +0100

    untabify draw.h and window.h and make sync

--- include/graphics/window.h
+++ include/graphics/window.h
@@ -6,38 +6,38 @@
 /*s: struct [[Screen]] */
 struct Screen
 {
-    Display	*display;	/* display holding data */
-    int		id;			/* id of system-held Screen */
+    Display *display;   /* display holding data */
+    int     id;         /* id of system-held Screen */
 
-    Image	*image;		/* unused; for reference only */
-    Image	*fill;		/* color to paint behind windows */
+    Image   *image;     /* unused; for reference only */
+    Image   *fill;      /* color to paint behind windows */
 };
 /*e: struct [[Screen]] */
 
 // set by initdraw() automatically.
-extern	Screen	*screen; // was called _screen before
+extern  Screen  *screen; // was called _screen before
 
 // called by initdraw()
-extern int	gengetwindow(Display*, char*, Image**, Screen**, int);
+extern int  gengetwindow(Display*, char*, Image**, Screen**, int);
 
 //
 // Base layer
 //
-extern Screen*	allocscreen(Image*, Image*, int);
-extern int	freescreen(Screen*);
-extern Screen*	publicscreen(Display*, int, ulong);
+extern Screen*  allocscreen(Image*, Image*, int);
+extern int      freescreen(Screen*);
+extern Screen*  publicscreen(Display*, int, ulong);
 
 /*
  * Windows
  */
-extern Image*	allocwindow(Screen*, Rectangle, int, ulong);
+extern Image*   allocwindow(Screen*, Rectangle, int, ulong);
 
-extern int	originwindow(Image*, Point, Point);
-extern void	bottomnwindows(Image**, int);
-extern void	bottomwindow(Image*);
-extern void	topnwindows(Image**, int);
-extern void	topwindow(Image*);
+extern int  originwindow(Image*, Point, Point);
+extern void bottomnwindows(Image**, int);
+extern void bottomwindow(Image*);
+extern void topnwindows(Image**, int);
+extern void topwindow(Image*);
 
-extern int	newwindow(char*);
+extern int  newwindow(char*);
 
 /*e: include/graphics/window.h */
