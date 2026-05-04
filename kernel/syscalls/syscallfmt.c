85098388a07d24e652fe851aa83bdfc4bc3dc9e4
pad <yoann.padioleau@gmail.com>
2026-05-04 13:45:25 +0200

    same

--- kernel/syscalls/syscallfmt.c
+++ kernel/syscalls/syscallfmt.c
@@ -77,8 +77,8 @@ syscallfmt(int syscallno, ulong pc, va_list list)
     if(syscallno > nsyscall)
         fmtprint(&fmt, " %d ", syscallno);
     else
-        //pad: was "%s " but I adjusted for my iwp9 ratrace demo
-        // of changing syscall number and seing the effect
+        //pad: was "%s " but I adjusted for my IWP9 ratrace demo
+        // of changing a syscall number and seing the effect
         fmtprint(&fmt, "%7s(%02d) ", sysctab[syscallno]?
             sysctab[syscallno]: "huh?", syscallno);
 
