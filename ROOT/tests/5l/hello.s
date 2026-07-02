5847d5f1ccd1ec8c6a141becc342c83e6c5e11f5
pad <yoann.padioleau@gmail.com>
2026-07-02 13:46:57 +0200

    update tests/5l/hello.s to latest syscall number

--- ROOT/tests/5l/hello.s
+++ ROOT/tests/5l/hello.s
@@ -9,7 +9,7 @@ TEXT _main(SB), $20
         MOVW $0, R1
         MOVW R1, 16(R13)
         MOVW R1, 20(R13)
-        MOVW $9 /*PWRITE*/, R0
+        MOVW $11 /*PWRITE*/, R0
         /* system call */
         SWI $0
         BL exit(SB)
