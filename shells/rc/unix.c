f8e20b870aea90b3f7294fd89aaa844d9217772a
pad <yoann.padioleau@gmail.com>
2026-07-02 18:10:42 +0200

    rc/unix.c: untabify

--- shells/rc/unix.c
+++ shells/rc/unix.c
@@ -22,7 +22,7 @@ char *Fdprefix = "/dev/fd/";
 // Environment
 //******************************************************************************
 
-#define	SEP	'\1'
+#define SEP '\1'
 
 word*
 enval(char *s)
@@ -65,7 +65,7 @@ Vinit(void)
       }
       *s='=';
       break;
-    case '(':	/* ignore functions for now */
+    case '(':   /* ignore functions for now */
       break;
     }
   }
@@ -85,9 +85,9 @@ Xrdfn(void)
     case '\0':
       pfmt(err, "environment %q?\n", *envp);
       break;
-    case '=':	/* ignore variables */
+    case '=':   /* ignore variables */
       break;
-    case '(':		/* Bourne again */
+    case '(':       /* Bourne again */
       s=*envp+3;
       envp++;
       len = strlen(s);
@@ -105,85 +105,85 @@ Xrdfn(void)
 //void
 //execfinit(void)
 //{
-//	static int first = 1;
-//	if(first){
-//		rdfns[0].i = 1;
-//		rdfns[1].f = Xrdfn;
-//		rdfns[2].f = Xjump;
-//		rdfns[3].i = 1;
-//		first = 0;
-//	}
-//	Xpopm();
-//	envp = environp;
-//	start(rdfns, 1, runq->local);
+//  static int first = 1;
+//  if(first){
+//      rdfns[0].i = 1;
+//      rdfns[1].f = Xrdfn;
+//      rdfns[2].f = Xjump;
+//      rdfns[3].i = 1;
+//      first = 0;
+//  }
+//  Xpopm();
+//  envp = environp;
+//  start(rdfns, 1, runq->local);
 //}
 //
 //int
 //cmpenv(const void *aa, const void *ab)
 //{
-//	char **a = aa, **b = ab;
+//  char **a = aa, **b = ab;
 //
-//	return strcmp(*a, *b);
+//  return strcmp(*a, *b);
 //}
 //
 //char **
 //mkenv(void)
 //{
-//	char **env, **ep, *p, *q;
-//	struct var **h, *v;
-//	struct word *a;
-//	int nvar = 0, nchr = 0, sep;
+//  char **env, **ep, *p, *q;
+//  struct var **h, *v;
+//  struct word *a;
+//  int nvar = 0, nchr = 0, sep;
 //
-//	/*
-//	 * Slightly kludgy loops look at locals then globals.
-//	 * locals no longer exist - geoff
-//	 */
-//	for(h = gvar-1; h != &gvar[NVAR]; h++)
-//	for(v = h >= gvar? *h: runq->local; v ;v = v->next){
-//		if((v==vlook(v->name)) && v->val){
-//			nvar++;
-//			nchr+=strlen(v->name)+1;
-//			for(a = v->val;a;a = a->next)
-//				nchr+=strlen(a->word)+1;
-//		}
-//		if(v->fn){
-//			nvar++;
-//			nchr+=strlen(v->name)+strlen(v->fn[v->pc-1].s)+8;
-//		}
-//	}
-//	env = (char **)emalloc((nvar+1)*sizeof(char *)+nchr);
-//	ep = env;
-//	p = (char *)&env[nvar+1];
-//	for(h = gvar-1; h != &gvar[NVAR]; h++)
-//	for(v = h >= gvar? *h: runq->local;v;v = v->next){
-//		if((v==vlook(v->name)) && v->val){
-//			*ep++=p;
-//			q = v->name;
-//			while(*q) *p++=*q++;
-//			sep='=';
-//			for(a = v->val;a;a = a->next){
-//				*p++=sep;
-//				sep = SEP;
-//				q = a->word;
-//				while(*q) *p++=*q++;
-//			}
-//			*p++='\0';
-//		}
-//		if(v->fn){
-//			*ep++=p;
-//			*p++='#'; *p++='('; *p++=')';	/* to fool Bourne */
-//			*p++='f'; *p++='n'; *p++=' ';
-//			q = v->name;
-//			while(*q) *p++=*q++;
-//			*p++=' ';
-//			q = v->fn[v->pc-1].s;
-//			while(*q) *p++=*q++;
-//			*p++='\0';
-//		}
-//	}
-//	*ep = 0;
-//	qsort((void *)env, nvar, sizeof ep[0], cmpenv);
-//	return env;	
+//  /*
+//   * Slightly kludgy loops look at locals then globals.
+//   * locals no longer exist - geoff
+//   */
+//  for(h = gvar-1; h != &gvar[NVAR]; h++)
+//  for(v = h >= gvar? *h: runq->local; v ;v = v->next){
+//      if((v==vlook(v->name)) && v->val){
+//          nvar++;
+//          nchr+=strlen(v->name)+1;
+//          for(a = v->val;a;a = a->next)
+//              nchr+=strlen(a->word)+1;
+//      }
+//      if(v->fn){
+//          nvar++;
+//          nchr+=strlen(v->name)+strlen(v->fn[v->pc-1].s)+8;
+//      }
+//  }
+//  env = (char **)emalloc((nvar+1)*sizeof(char *)+nchr);
+//  ep = env;
+//  p = (char *)&env[nvar+1];
+//  for(h = gvar-1; h != &gvar[NVAR]; h++)
+//  for(v = h >= gvar? *h: runq->local;v;v = v->next){
+//      if((v==vlook(v->name)) && v->val){
+//          *ep++=p;
+//          q = v->name;
+//          while(*q) *p++=*q++;
+//          sep='=';
+//          for(a = v->val;a;a = a->next){
+//              *p++=sep;
+//              sep = SEP;
+//              q = a->word;
+//              while(*q) *p++=*q++;
+//          }
+//          *p++='\0';
+//      }
+//      if(v->fn){
+//          *ep++=p;
+//          *p++='#'; *p++='('; *p++=')';   /* to fool Bourne */
+//          *p++='f'; *p++='n'; *p++=' ';
+//          q = v->name;
+//          while(*q) *p++=*q++;
+//          *p++=' ';
+//          q = v->fn[v->pc-1].s;
+//          while(*q) *p++=*q++;
+//          *p++='\0';
+//      }
+//  }
+//  *ep = 0;
+//  qsort((void *)env, nvar, sizeof ep[0], cmpenv);
+//  return env; 
 //}
 
 void
@@ -273,14 +273,14 @@ Waitfor(int pid, bool persist)
 }
 
 char *signame[] = {
-  "sigexit",	"sighup",	"sigint",	"sigquit",
-  "sigill",	"sigtrap",	"sigiot",	"sigemt",
-  "sigfpe",	"sigkill",	"sigbus",	"sigsegv",
-  "sigsys",	"sigpipe",	"sigalrm",	"sigterm",
-  "sig16",	"sigstop",	"sigtstp",	"sigcont",
-  "sigchld",	"sigttin",	"sigttou",	"sigtint",
-  "sigxcpu",	"sigxfsz",	"sig26",	"sig27",
-  "sig28",	"sig29",	"sig30",	"sig31",
+  "sigexit",    "sighup",   "sigint",   "sigquit",
+  "sigill", "sigtrap",  "sigiot",   "sigemt",
+  "sigfpe", "sigkill",  "sigbus",   "sigsegv",
+  "sigsys", "sigpipe",  "sigalrm",  "sigterm",
+  "sig16",  "sigstop",  "sigtstp",  "sigcont",
+  "sigchld",    "sigttin",  "sigttou",  "sigtint",
+  "sigxcpu",    "sigxfsz",  "sig26",    "sig27",
+  "sig28",  "sig29",    "sig30",    "sig31",
   0,
 };
 
@@ -306,7 +306,7 @@ Trapinit(void)
   int i;
   void (*sig)();
 
-  if(1 || flag['d']){	/* wrong!!! */
+  if(1 || flag['d']){   /* wrong!!! */
     sig = signal(SIGINT, gettrap);
     if(sig==SIG_IGN)
       signal(SIGINT, SIG_IGN);
@@ -339,7 +339,7 @@ Noerror(void)
 // Directories
 //******************************************************************************
 
-#define	NDIR	14		/* should get this from param.h */
+#define NDIR    14      /* should get this from param.h */
 int
 Globsize(char *p)
 {
@@ -360,7 +360,7 @@ Globsize(char *p)
 #include <sys/types.h>
 #include <dirent.h>
 
-#define	NDIRLIST	50
+#define NDIRLIST    50
 
 DIR *dirlist[NDIRLIST];
 
@@ -402,58 +402,58 @@ Closedir(int f)
 //mkargv(a)
 //register struct word *a;
 //{
-//	char **argv = (char **)emalloc((count(a)+2)*sizeof(char *));
-//	char **argp = argv+1;	/* leave one at front for runcoms */
+//  char **argv = (char **)emalloc((count(a)+2)*sizeof(char *));
+//  char **argp = argv+1;   /* leave one at front for runcoms */
 //
-//	for(;a;a = a->next)
-//		*argp++=a->word;
-//	*argp = 0;
-//	return argv;
+//  for(;a;a = a->next)
+//      *argp++=a->word;
+//  *argp = 0;
+//  return argv;
 //}
 //
 
 //void
 //Execute(struct word *args, struct word *path)
 //{
-//	char *msg="not found";
-//	int txtbusy = 0;
-//	char **env = mkenv();
-//	char **argv = mkargv(args);
-//	char file[512];
+//  char *msg="not found";
+//  int txtbusy = 0;
+//  char **env = mkenv();
+//  char **argv = mkargv(args);
+//  char file[512];
 //
-//	for(;path;path = path->next){
-//		strcpy(file, path->word);
-//		if(file[0])
-//			strcat(file, "/");
-//		strcat(file, argv[1]);
+//  for(;path;path = path->next){
+//      strcpy(file, path->word);
+//      if(file[0])
+//          strcat(file, "/");
+//      strcat(file, argv[1]);
 //ReExec:
-//		execve(file, argv+1, env);
-//		switch(errno){
-//		case ENOEXEC:
-//			pfmt(err, "%s: Bourne again\n", argv[1]);
-//			argv[0]="sh";
-//			argv[1] = strdup(file);
-//			execve("/bin/sh", argv, env);
-//			goto Bad;
-//		case ETXTBSY:
-//			if(++txtbusy!=5){
-//				sleep(txtbusy);
-//				goto ReExec;
-//			}
-//			msg="text busy"; goto Bad;
-//		case EACCES:
-//			msg="no access";
-//			break;
-//		case ENOMEM:
-//			msg="not enough memory"; goto Bad;
-//		case E2BIG:
-//			msg="too big"; goto Bad;
-//		}
-//	}
+//      execve(file, argv+1, env);
+//      switch(errno){
+//      case ENOEXEC:
+//          pfmt(err, "%s: Bourne again\n", argv[1]);
+//          argv[0]="sh";
+//          argv[1] = strdup(file);
+//          execve("/bin/sh", argv, env);
+//          goto Bad;
+//      case ETXTBSY:
+//          if(++txtbusy!=5){
+//              sleep(txtbusy);
+//              goto ReExec;
+//          }
+//          msg="text busy"; goto Bad;
+//      case EACCES:
+//          msg="no access";
+//          break;
+//      case ENOMEM:
+//          msg="not enough memory"; goto Bad;
+//      case E2BIG:
+//          msg="too big"; goto Bad;
+//      }
+//  }
 //Bad:
-//	pfmt(err, "%s: %s\n", argv[1], msg);
-//	efree((char *)env);
-//	efree((char *)argv);
+//  pfmt(err, "%s: %s\n", argv[1], msg);
+//  efree((char *)env);
+//  efree((char *)argv);
 //}
 
 
@@ -484,30 +484,30 @@ bool Isatty(fdt fd){
 }
 
 //void
-//execumask(void)		/* wrong -- should fork before writing */
+//execumask(void)       /* wrong -- should fork before writing */
 //{
-//	int m;
-//	struct io out[1];
-//	switch(count(runq->argv->words)){
-//	default:
-//		pfmt(err, "Usage: umask [umask]\n");
-//		setstatus("umask usage");
-//		poplist();
-//		return;
-//	case 2:
-//		umask(octal(runq->argv->words->next->word));
-//		break;
-//	case 1: 
-//		umask(m = umask(0));
-//		out->fd = mapfd(1);
-//		out->bufp = out->buf;
-//		out->ebuf=&out->buf[NBUF];
-//		out->strp = 0;
-//		pfmt(out, "%o\n", m);
-//		break;
-//	}
-//	setstatus("");
-//	poplist();
+//  int m;
+//  struct io out[1];
+//  switch(count(runq->argv->words)){
+//  default:
+//      pfmt(err, "Usage: umask [umask]\n");
+//      setstatus("umask usage");
+//      poplist();
+//      return;
+//  case 2:
+//      umask(octal(runq->argv->words->next->word));
+//      break;
+//  case 1: 
+//      umask(m = umask(0));
+//      out->fd = mapfd(1);
+//      out->bufp = out->buf;
+//      out->ebuf=&out->buf[NBUF];
+//      out->strp = 0;
+//      pfmt(out, "%o\n", m);
+//      break;
+//  }
+//  setstatus("");
+//  poplist();
 //}
 
 
@@ -530,23 +530,23 @@ needsrcquote(int c)
 //void
 //delwaitpid(int pid)
 //{
-//	int r, w;
-//	
-//	for(r=w=0; r<nwaitpids; r++)
-//		if(waitpids[r] != pid)
-//			waitpids[w++] = waitpids[r];
-//	nwaitpids = w;
+//  int r, w;
+//  
+//  for(r=w=0; r<nwaitpids; r++)
+//      if(waitpids[r] != pid)
+//          waitpids[w++] = waitpids[r];
+//  nwaitpids = w;
 //}
 
 //int
 //havewaitpid(int pid)
 //{
-//	int i;
+//  int i;
 //
-//	for(i=0; i<nwaitpids; i++)
-//		if(waitpids[i] == pid)
-//			return 1;
-//	return 0;
+//  for(i=0; i<nwaitpids; i++)
+//      if(waitpids[i] == pid)
+//          return 1;
+//  return 0;
 //
 //}
 
